116d063e00f259cc1d884a14250fc07d4c2715bb
Andrew Peck <andrew.peck@cern.ch>
2020-10-28 09:28:45 -0400

    Line fill mode

--- config.el
+++ config.el
@@ -34,7 +34,59 @@
 (add-hook 'LaTeX-mode-hook (lambda () (reftex-mode 1)))
 (add-hook 'reftex-toc-mode-hook (lambda ()
                                   (define-key reftex-toc-mode-map (kbd "<return>") 'reftex-toc-goto-line)))
-;;;;;
+
+
+;; https://abizjak.github.io/emacs/2016/03/06/latex-fill-paragraph.html
+(defun line-fill-paragraph (&optional P)
+  "When called with prefix argument call `fill-paragraph'.
+   Otherwise split the current paragraph into one sentence per line."
+  (interactive "P")
+  (if (not P)
+      (save-excursion
+        (let ((fill-column 12345678)) ;; relies on dynamic binding
+          (fill-paragraph) ;; this will not work correctly if the paragraph is
+          ;; longer than 12345678 characters (in which case the
+          ;; file must be at least 12MB long. This is unlikely.)
+          (let ((end (save-excursion
+                       (forward-paragraph 1)
+                       (backward-sentence)
+                       (point-marker))))  ;; remember where to stop
+            (beginning-of-line)
+            (while (progn (forward-sentence)
+                          (<= (point) (marker-position end)))
+              (just-one-space) ;; leaves only one space, point is after it
+              (delete-char -1) ;; delete the space
+              (newline)        ;; and insert a newline
+              (LaTeX-indent-line) ;; I only use this in combination with late, so this makes sense
+              ))))
+
+    ;; otherwise do ordinary fill paragraph
+    (fill-paragraph P)))
+
+;;(add-hook 'LaTex-mode-hook
+  ;(lambda () (evil-local-set-key 'normal (kbd "M-q") 'line-fill-paragraph))
+  ;(lambda () (define-key evil-normal-state-local-map (kbd "M-q") 'line-fill-paragraph))
+  ;)
+(evil-define-minor-mode-key 'normal 'latex-mode-map (kbd "M-q") #'line-fill-paragraph)
+
+(defun electric-space () ; Trying to get Emacs to do semantic linefeeds
+  (interactive)
+  (if (looking-back (sentence-end))
+      (insert "\n")
+       (self-insert-command 1))
+       )
+
+(defvar electric-space-on-p nil)
+
+(defun toggle-electric-space ()
+  (interactive)
+  (global-set-key
+   " "
+   (if (setq electric-space-on-p
+             (not electric-space-on-p))
+       'electric-space
+     'self-insert-command)))
+
 ;;;;; (setq speedbar-show-unknown-files t) ; show all files
 ;;;;; (setq speedbar-use-images nil) ; use text for buttons
 ;;;;;                                         ;(setq sr-speedbar-right-side nil) ; put on left side
