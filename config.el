d311ca7caa7e6906bf1138009493f66c7e656262
Andrew Peck <andrew.peck@cern.ch>
2021-01-18 13:10:41 -0500

    emacs read process depth for lsp

--- config.el
+++ config.el
@@ -7,6 +7,11 @@
 ;; Random
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+
+;; Increase the amount of data which Emacs reads from the process. Again the emacs default is too low 4k considering that the some of the language server responses are in 800k - 3M range.
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
+
 ;; Etags search depth
 (setq etags-table-search-up-depth 10)
 
