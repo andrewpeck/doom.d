0141011092f83acd9988402185473e0026bd4e03
Andrew Peck <andrew.peck@cern.ch>
2021-02-21 19:59:16 -0500

    move to archive

--- config.el
+++ config.el
@@ -925,78 +925,6 @@
 
   )
 
-;;   ;; SciMax Org Return
-;;   ;; http://kitchingroup.cheme.cmu.edu/blog/2017/04/09/A-better-return-in-org-mode/
-;;   ;;(require 'org-inlinetask)
-;;   (defun scimax/org-return (&optional ignore)
-;;     "Add new list item, heading or table row with RET.
-;;   A double return on an empty element deletes it.
-;;   Use a prefix arg to get regular RET. "
-;;     (interactive "P")
-;;     (if ignore
-;;         (org-return)
-;;       (cond
-;;
-;;        ((eq 'line-break (car (org-element-context)))
-;;         (org-return-indent))
-;;
-;;        ;; Open links like usual, unless point is at the end of a line.
-;;        ;; and if at beginning of line, just press enter.
-;;        ((or (and (eq 'link (car (org-element-context))) (not (eolp)))
-;;             (bolp))
-;;         (org-return))
-;;
-;;        ;; It doesn't make sense to add headings in inline tasks. Thanks Anders
-;;        ;; Johansson!
-;;        ((org-inlinetask-in-task-p)
-;;         (org-return))
-;;
-;;        ;; checkboxes too
-;;        ((org-at-item-checkbox-p)
-;;         (org-insert-todo-heading nil))
-;;
-;;        ;; lists end with two blank lines, so we need to make sure we are also not
-;;        ;; at the beginning of a line to avoid a loop where a new entry gets
-;;        ;; created with only one blank line.
-;;        ((org-in-item-p)
-;;         (if (save-excursion (beginning-of-line) (org-element-property :contents-begin (org-element-context)))
-;;             (org-insert-heading)
-;;           (beginning-of-line)
-;;           (delete-region (line-beginning-position) (line-end-position))
-;;           (org-return)))
-;;
-;;        ;; org-heading
-;;        ((org-at-heading-p)
-;;         (if (not (string= "" (org-element-property :title (org-element-context))))
-;;             (progn (org-end-of-meta-data)
-;;                    (org-insert-heading-respect-content)
-;;                    (outline-show-entry))
-;;           (beginning-of-line)
-;;           (setf (buffer-substring
-;;                  (line-beginning-position) (line-end-position)) "")))
-;;
-;;        ;; tables
-;;        ((org-at-table-p)
-;;         (if (-any?
-;;              (lambda (x) (not (string= "" x)))
-;;              (nth
-;;               (- (org-table-current-dline) 1)
-;;               (org-table-to-lisp)))
-;;             (org-return)
-;;           ;; empty row
-;;           (beginning-of-line)
-;;           (setf (buffer-substring
-;;                  (line-beginning-position) (line-end-position)) "")
-;;           (org-return)))
-;;
-;;        ;; fall-through case
-;;        (t
-;;         (org-return)))))
-
-
-;;(define-key org-mode-map (kbd "RET")
-;;  'scimax/org-return)
-
 ;;------------------------------------------------------------------------------
 ;; Org Image Attach
 ;;------------------------------------------------------------------------------
@@ -1356,6 +1284,73 @@
   ;;                                       company-files
   ;;                                      company-keywords company-capf company-dabbrev-code
   ;;                                      company-etags company-dabbrev))
+;;   ;; SciMax Org Return
+;;   ;; http://kitchingroup.cheme.cmu.edu/blog/2017/04/09/A-better-return-in-org-mode/
+;;   ;;(require 'org-inlinetask)
+;;   (defun scimax/org-return (&optional ignore)
+;;     "Add new list item, heading or table row with RET.
+;;   A double return on an empty element deletes it.
+;;   Use a prefix arg to get regular RET. "
+;;     (interactive "P")
+;;     (if ignore
+;;         (org-return)
+;;       (cond
+;;
+;;        ((eq 'line-break (car (org-element-context)))
+;;         (org-return-indent))
+;;
+;;        ;; Open links like usual, unless point is at the end of a line.
+;;        ;; and if at beginning of line, just press enter.
+;;        ((or (and (eq 'link (car (org-element-context))) (not (eolp)))
+;;             (bolp))
+;;         (org-return))
+;;
+;;        ;; It doesn't make sense to add headings in inline tasks. Thanks Anders
+;;        ;; Johansson!
+;;        ((org-inlinetask-in-task-p)
+;;         (org-return))
+;;
+;;        ;; checkboxes too
+;;        ((org-at-item-checkbox-p)
+;;         (org-insert-todo-heading nil))
+;;
+;;        ;; lists end with two blank lines, so we need to make sure we are also not
+;;        ;; at the beginning of a line to avoid a loop where a new entry gets
+;;        ;; created with only one blank line.
+;;        ((org-in-item-p)
+;;         (if (save-excursion (beginning-of-line) (org-element-property :contents-begin (org-element-context)))
+;;             (org-insert-heading)
+;;           (beginning-of-line)
+;;           (delete-region (line-beginning-position) (line-end-position))
+;;           (org-return)))
+;;
+;;        ;; org-heading
+;;        ((org-at-heading-p)
+;;         (if (not (string= "" (org-element-property :title (org-element-context))))
+;;             (progn (org-end-of-meta-data)
+;;                    (org-insert-heading-respect-content)
+;;                    (outline-show-entry))
+;;           (beginning-of-line)
+;;           (setf (buffer-substring
+;;                  (line-beginning-position) (line-end-position)) "")))
+;;
+;;        ;; tables
+;;        ((org-at-table-p)
+;;         (if (-any?
+;;              (lambda (x) (not (string= "" x)))
+;;              (nth
+;;               (- (org-table-current-dline) 1)
+;;               (org-table-to-lisp)))
+;;             (org-return)
+;;           ;; empty row
+;;           (beginning-of-line)
+;;           (setf (buffer-substring
+;;                  (line-beginning-position) (line-end-position)) "")
+;;           (org-return)))
+;;
+;;        ;; fall-through case
+;;        (t
+;;         (org-return)))))
 
   ;;(setq company-frontends '(company-preview-if-just-one-frontend company-box-frontend company-echo-metadata-frontend))
   ;;(setq company-frontends '(company-preview-if-just-one-frontend  company-echo-metadata-frontend))
@@ -1364,6 +1359,8 @@
 
   ;;(define-key company-active-map (kbd "<return>") nil)
   ;;(define-key company-active-map (kbd "<tab>") #'company-complete-selection )
+;;(define-key org-mode-map (kbd "RET")
+;;  'scimax/org-return)
 
   ;;   (set-company-backend! '(company-tabnine company-yasnippet  company-files))
   ;;                                         ;(set-company-backend! 'org-mode '(company-roam company-files company-dabbrev))
