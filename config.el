5961bae6a2903cbb48410168232b904367abf1c2
Andrew Peck <andrew.peck@cern.ch>
2021-02-21 19:57:10 -0500

    Git fringe settings

--- config.el
+++ config.el
@@ -258,39 +258,46 @@
 ;; Git Gutter
 ;;------------------------------------------------------------------------------
 
-(after! fringe-helper
-  (fringe-mode 9)
-  (fringe-helper-define 'git-gutter-fr:added nil
-    "....X...."
-    "....X...."
-    "....X...."
-    "....X...."
-    "XXXXXXXXX"
-    "....X...."
-    "....X...."
-    "....X....")
-
-  (fringe-helper-define 'git-gutter-fr:deleted nil
-    "........."
-    "........."
-    "........."
-    "..XXXXXX."
-    "........."
-    "........."
-    "........."
-    ".........")
-
-  (fringe-helper-define 'git-gutter-fr:modified nil
-    "........."
-    "...X....."
-    "...XXX..."
-    "...XXXX.."
-    "...XXXXX."
-    "...XXXX.."
-    "...XXX..."
-    "...X.....")
-  )
-
+;; for some reason this is slow to load :(
+;;
+;;(use-package! git-gutter
+;;  :defer t
+;;  :init
+;;  (use-package! git-gutter-fringe
+;;    :defer t
+;;    :config
+;;    (fringe-mode 9)
+;;    (fringe-helper-define 'git-gutter-fr:added nil
+;;      "....X...."
+;;      "....X...."
+;;      "....X...."
+;;      "....X...."
+;;      "XXXXXXXXX"
+;;      "....X...."
+;;      "....X...."
+;;      "....X....")
+;;
+;;    (fringe-helper-define 'git-gutter-fr:deleted nil
+;;      "........."
+;;      "........."
+;;      "........."
+;;      "..XXXXXX."
+;;      "........."
+;;      "........."
+;;      "........."
+;;      ".........")
+;;
+;;    (fringe-helper-define 'git-gutter-fr:modified nil
+;;      "........."
+;;      "...X....."
+;;      "...XXX..."
+;;      "...XXXX.."
+;;      "...XXXXX."
+;;      "...XXXX.."
+;;      "...XXX..."
+;;      "...X.....")
+;;    )
+;;)
 
 ;;------------------------------------------------------------------------------
 ;;;;; Rainbow Delimeters
