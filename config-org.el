fcad3201e2fddafd404861c345f2ae02122b6e1f
Andrew Peck <andrew.peck@cern.ch>
2021-09-15 20:00:06 -0400

    Disable roam server

--- config-org.el
+++ config-org.el
@@ -299,20 +299,20 @@
         )
 
   (setq org-roam-link-title-format "Org:%s")
-  (use-package! org-roam-server
-    :ensure t
-    :config
-    (setq org-roam-server-host "127.0.0.1"
-          org-roam-server-port 8080
-          org-roam-server-authenticate nil
-          org-roam-server-export-inline-images t
-          org-roam-server-serve-files nil
-          org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
-          org-roam-server-network-poll t
-          org-roam-server-network-arrows nil
-          org-roam-server-network-label-truncate t
-          org-roam-server-network-label-truncate-length 60
-          org-roam-server-network-label-wrap-length 20))
+  ;; (use-package! org-roam-server
+  ;;   :ensure t
+  ;;   :config
+  ;;   (setq org-roam-server-host "127.0.0.1"
+  ;;         org-roam-server-port 8080
+  ;;         org-roam-server-authenticate nil
+  ;;         org-roam-server-export-inline-images t
+  ;;         org-roam-server-serve-files nil
+  ;;         org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
+  ;;         org-roam-server-network-poll t
+  ;;         org-roam-server-network-arrows nil
+  ;;         org-roam-server-network-label-truncate t
+  ;;         org-roam-server-network-label-truncate-length 60
+  ;;         org-roam-server-network-label-wrap-length 20))
 
   (setq org-roam-db-location "~/.org-roam.db"))
 
