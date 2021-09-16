8bbb7b7718b5804aed772ed21734dfadd6df40b4
Andrew Peck <andrew.peck@cern.ch>
2021-09-15 20:06:10 -0400

    lsp tweaks

--- config-lsp.el
+++ config-lsp.el
@@ -20,44 +20,90 @@
 (add-hook 'vhdl-mode-hook #'lsp)
 (add-hook 'vhdl-mode-hook #'lsp-ui-mode)
 
+(add-hook 'python-mode-hook #'lsp-mode)
+(add-hook 'python-mode-hook #'lsp-ui-mode)
+
+
 (after! lsp-mode
-  (setq lsp-progress-via-spinner nil)
-  (setq  ccls-sem-highlight-method nil)
-  (setq lsp-enable-on-type-formatting nil)
-  (setq lsp-enable-indentation nil)
-  (setq +format-with-lsp nil)
 
-  (add-hook 'python-mode-hook #'lsp-mode)
-  (add-hook 'python-mode-hook #'lsp-ui-mode)
-
-  ;; VHDL Tool
+  ;; vhdl-tool, hdl-checker vhdl-ls ghdl-ls
   (setq lsp-vhdl-server 'vhdl-tool)
-  (setq lsp-vhdl-server-path "vhdl-tool")
-  ;; (setq lsp-vhdl-server 'vhdl-ls)
-  ;; (setq lsp-vhdl-server-path "~/bin/vhdl_ls")
-  )
+  (cl-case lsp-vhdl-server
+    ('vhdl-tool (setq lsp-vhdl-server-path (executable-find "vhdl-tool")))
+    ('ghdl-ls (setq lsp-vhdl-server-path (executable-find "ghdl-ls")))
+    ('vhdl-ls (setq lsp-vhdl-server-path (executable-find "vhdl_ls"))))
+
+  (setq lsp-progress-via-spinner nil
+        ccls-sem-highlight-method nil
+        lsp-enable-on-type-formatting nil
+        lsp-enable-indentation nil
+        +format-with-lsp nil)
+
+  ) ;; after! lsp
 
 
-;; HDL Checker
-;;(setq lsp-vhdl-server 'hdl-checker)
+;; (require 'lsp)
+;; (add-hook 'vhdl-mode-hook #'lsp)
+;; (add-to-list 'lsp-language-id-configuration '(vhdl-mode . "vhdl"))
+;; (lsp-register-client
+;;  (make-lsp-client :new-connection (lsp-stdio-connection '("ghdl-ls"))
+;;                   :major-modes '(vhdl-mode)
+;;                   :priority -1
+;;                   :server-id 'lsp-vhdl-mode))
 
-;; VHDL Ls (Rust HDL)
-;;(setq lsp-vhdl-server 'vhdl-ls)
-;;(setq lsp-vhdl-server-path "~/rust_hdl/target/release/vhdl_ls")
+;; (setq lsp-log-io nil
+;;       lsp-auto-configure t
+;;       lsp-auto-guess-root t
+;;       lsp-completion-enable t
+;;       lsp-enable-xref t
+;;       lsp-prefer-flymake nil
+;;       lsp-use-native-json t
+;;       lsp-enable-indentation t
+;;       lsp-response-timeout 10
+;;       lsp-restart 'auto-restart
+;;       lsp-keep-workspace-alive t
+;;       lsp-eldoc-render-all nil
+;;       lsp-enable-snippet nil
+;;       lsp-enable-folding t
+;;       )
 
-;;(use-package lsp-mode
-;;         :config
-;;         (add-hook 'vhdl-mode-hook 'lsp))
+;; ;; lsp-ui gives us the blue documentation boxes and the sidebar info
+;; (setq lsp-ui-sideline-ignore-duplicate t
+;;       lsp-ui-sideline-delay 0.5
+;;       lsp-ui-sideline-show-symbol t
+;;       lsp-ui-sideline-show-hover t
+;;       lsp-ui-sideline-show-diagnostics t
+;;       lsp-ui-sideline-show-code-actions t
+;;       lsp-ui-peek-always-show t
+;;       lsp-ui-doc-use-childframe t)
 
-;; (flycheck-define-checker vhdl-tool
-;;   "A VHDL syntax checker, type checker and linter using VHDL-Tool.
-;;         See URL `http://vhdltool.com'."
-;;   :command ("vhdl-tool" "client" "lint" "--compact" "--stdin" "-f" source
-;;             )
-;;   :standard-input t
-;;   :error-patterns
-;;   ((warning line-start (file-name) ":" line ":" column ":w:" (message) line-end)
-;;    (error line-start (file-name) ":" line ":" column ":e:" (message) line-end))
-;;   :modes (vhdl-mode))
-;;
-;; (add-to-list 'flycheck-checkers 'vhdl-tool)
+;; (lsp-ui-flycheck-enable t)
+;; (lsp-ui-sideline-enable t)
+;; (lsp-ui-imenu-enable t)
+;; (lsp-lens-mode t)
+;; (lsp-ui-peek-enable t)
+;; (lsp-ui-doc-enable t)
+
+;;   ;;; company lsp
+;; ;; install LSP company backend for LSP-driven completion
+;; (setq company-lsp-cache-candidates t
+;;       company-lsp-enable-recompletion t
+;;       company-lsp-enable-snippet t
+;;       company-lsp-async t)
+
+;; (setq lsp-vhdl-server 'ghdl-ls
+;;       lsp-vhdl-server-path (executable-find "ghdl-ls")
+;;       lsp-vhdl--params nil)
+
+;; ;; (flycheck-define-checker vhdl-tool
+;; ;;   "A VHDL syntax checker, type checker and linter using VHDL-Tool.
+;; ;;         See URL `http://vhdltool.com'."
+;; ;;   :command ("vhdl-tool" "client" "lint" "--compact" "--stdin" "-f" source
+;; ;;             )
+;; ;;   :standard-input t
+;; ;;   :error-patterns
+;; ;;   ((warning line-start (file-name) ":" line ":" column ":w:" (message) line-end)
+;; ;;    (error line-start (file-name) ":" line ":" column ":e:" (message) line-end))
+;; ;;   :modes (vhdl-mode))
+;; ;;
+;; ;; (add-to-list 'flycheck-checkers 'vhdl-tool)
