081cc90fb51d28a13a1b48de974efaf155fb72f4
Andrew Peck <andrew.peck@cern.ch>
2022-01-27 08:57:34 -0500

    use vertico

--- init.el
+++ init.el
@@ -25,7 +25,9 @@
         +childframe)
         ;;helm              ; the *other* search engine for love and life
         ;;ido               ; the other *other* search engine...
-        (ivy +fuzzy)       ; a search engine for love and life
+        ;; (ivy +fuzzy)
+                                        ; a search engine for love and life
+        (vertico)
 
     :ui
         ;;deft           ; notational velocity for Emacs
