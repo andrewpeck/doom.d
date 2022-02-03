48387f132f2a19a720e269d1846608949aae9157
Andrew Peck <andrew.peck@cern.ch>
2022-02-02 19:13:11 -0500

    this was moved into another file

--- config.el
+++ config.el
@@ -74,17 +74,6 @@
 (after! writegood
   (writegood-passive-voice-turn-off))
 
-;; Save user defined words to the dictionary
-(after! ispell
-  (setq ispell-personal-dictionary "~/.aspell.en.pws")
-  (defun my-save-word () (interactive)
-         (let ((current-location (point)) (word (flyspell-get-word)))
-           (when (consp word)
-             (flyspell-do-correct 'save nil
-                                  (car word) current-location (cadr word)
-                                  (caddr word) current-location)))))
-
-;;------------------------------------------------------------------------------
 ;;; Snippets
 ;;------------------------------------------------------------------------------
 
