4e88c090feb51ed88802f1f459f6aa8738dc61a0
Andrew Peck <andrew.peck@cern.ch>
2022-05-13 18:28:49 -0400

    latex config

--- config-tex.el
+++ config-tex.el
@@ -1,13 +1,30 @@
 ;; -*- lexical-binding: t; -*-
 
+;; https://github.com/hlissner/doom-emacs/blob/master/modules/lang/latex/README.org
+(setq +latex-viewers
+      '(okular atril evince zathura))
+
 ;; https://emacs.stackexchange.com/questions/33663/in-auctex-how-could-i-fold-acronyms
 (after! tex-fold
   (add-to-list 'TeX-fold-macro-spec-list '("{1}" ("gls")))
   (add-to-list 'TeX-fold-macro-spec-list '("{1}" ("cite")))
+;;(add-to-list 'TeX-fold-macro-spec-list '("{1}s" ("gls")))
+;;(add-to-list 'TeX-fold-macro-spec-list '("{1}" ("ac" "acf")))
+;;(add-to-list 'TeX-fold-macro-spec-list '("{1}s" ("acp" "acpf")))
 )
+
+(add-hook
+ 'LaTex-mode-hook
+ (lambda () (flycheck-add-next-checker 'proselint 'tex-chktex)))
+
+;; this is some e.g. text that has an example in it. And some other sentence that is actually a sentence. Something else.
+
 ;; Semantic Linefeeds
 ;;------------------------------------------------------------------------------
 ;; https://abizjak.github.io/emacs/2016/03/06/latex-fill-paragraph.html
+;; TODO: check for common things at end of line:
+;; c.f. e.g. i.e.
+
 (defun ap/line-fill-paragraph (&optional P)
   "When called with prefix argument call `fill-paragraph'.
    Otherwise split the current paragraph into one sentence per line."
@@ -33,11 +50,8 @@
     ;; otherwise do ordinary fill paragraph
     (fill-paragraph P)))
 
-(evil-define-minor-mode-key 'normal 'latex-mode-map (kbd "M-q") #'ap/line-fill-paragraph)
-
-;;(add-hook 'LaTex-mode-hook
-;;          (lambda () (define-key evil-normal-state-local-map (kbd "M-q") 'ap/line-fill-paragraph))
-;;          )
+  (evil-define-key 'normal latex-mode-map (kbd "M-q") #'ap/line-fill-paragraph)
+  (evil-define-key 'normal latex-mode-map (kbd "M-q") nil)
 
 ;; TeX
 ;;------------------------------------------------------------------------------
