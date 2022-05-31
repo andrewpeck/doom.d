76083bf9b140a45f6790c3035106505e32ca8d1c
Andrew Peck <andrew.peck@cern.ch>
2022-05-31 19:58:01 -0400

    company settings

--- config-company.el
+++ config-company.el
@@ -6,9 +6,7 @@
 (after! company
 
   (setq company-idle-delay 0.5)
-
-  ;; ;; turn on company
-  ;; (add-hook 'after-init-hook 'global-company-mode)
+  (add-hook 'after-init-hook 'global-company-mode) ; turn on company
 
   ;; (define-key company-active-map (kbd "<tab>") nil)
   ;; (define-key company-active-map (kbd "<return>") #'company-complete-selection)
@@ -23,8 +21,6 @@
   ;; (add-hook 'company-mode-hook 'company-box-mode)
   ;; (setq-default company-box-backends-colors 'nil)
 
-  ;; (add-hook 'python-mode-hook (lambda () (add-to-list 'company-backends 'company-jedi)))
-
   ;; (add-hook 'Latex-mode-hook
   ;;           (lambda ()
   ;;             (set-company-backend! 'LaTeX-mode-hook
@@ -33,24 +29,22 @@
   ;;                                   company-keywords company-capf company-dabbrev-code
   ;;                                   company-etags company-dabbrev))))
 
-  ;; ;; (set-company-backend! 'org-mode '(company-yasnippet
-  ;; ;;                                   company-capf
-  ;; ;;                                   company-files
-  ;; ;;                                   company-keywords
-  ;; ;;                                   ))
+  ;; company-files
+  ;; company-yasnippet
+  ;; company-capf
 
-  ;; (set-company-backend!
-  ;;   '(text-mode markdown-mode org-mode gfm-mode)
-  ;;   '(:seperate company-files company-yasnippet company-ispell))
-
+  ;; (set-company-backend! 'org-mode nil)
   (set-company-backend!
-    '(vhdl-mode)
-    '(company-yasnippet company-keywords company-capf company-files
-                        company-dabbrev-code company-etags company-dabbrev ))
+    '(markdown-mode org-mode)
+    '(company-files company-ispell))
 
+  ;; (set-company-backend! 'vhdl-mode nil)
   ;; (set-company-backend!
-  ;;   '(prog-mode tcl-mode python-mode vhdl-mode)
-  ;;   '(company-yasnippet company-keywords company-capf company-files
-  ;;                       company-dabbrev-code company-etags company-dabbrev ))
+  ;;   '(vhdl-mode)
+  ;;   '(company-capf company-keywords company-dabbrev company-yasnippet))
 
+  (set-company-backend! 'hog-src-mode nil)
+  (set-company-backend!
+    '(hog-src-mode)
+    '(company-files company-dabbrev))
   )
