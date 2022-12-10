4c998546b935333b6cb1991843a5cc35f3c62a55
Andrew Peck <andrew.peck@cern.ch>
2022-12-10 10:30:58 -0500

    keep gerbview from polluting the pwd

--- gerb-view.el
+++ gerb-view.el
@@ -1,13 +1,16 @@
 ;;; -*- lexical-binding: t; -*-
 
-;; TODO: should copy the buffer to a temp file
+;; TODO: should copy the buffer to a temp file.. sometimes e.g. if trying to open a zipped file then
+;; trying to exec gerbv fails
 
 ;;  convert gerber to svg
 (defun gerber->svg--file (filename)
-  (let ((tmp-name (concat (make-temp-file (concat filename "-")) ".svg")))
-    (shell-command  (concat  "gerbv --dpi=600 --border=0 --export=svg "
-                             "--output=" tmp-name " " filename))
-    tmp-name))
+  (let* ((tmp-name
+          (format  "%s.svg" (make-temp-file (concat (file-name-base filename) "-"))))
+         (command
+          (format  "gerbv --dpi=600 --border=0 --export=svg --output=%s %s" tmp-name filename)))
+    (message tmp-name)
+    (shell-command  command) tmp-name))
 
 (defun gerber->svg ()
   (interactive)
