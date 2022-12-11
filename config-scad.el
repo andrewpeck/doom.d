38a43b4f75938082463d91fbcbba9740f00dd3ae
Andrew Peck <andrew.peck@cern.ch>
2022-12-11 10:17:48 -0500

    make scad call through setsid, add shortcut for cheatsheet

--- config-scad.el
+++ config-scad.el
@@ -19,9 +19,16 @@
 
 ;; Function to open the current file in openscad
 (defun open-in-openscad ()
+  "Open the current buffer in openscad"
   (interactive)
-  (start-process (format "*scad-%s*" (file-name-base))
-                 nil "openscad" (buffer-file-name)))
+  (start-process (format "*scad-%s*" (file-name-base (buffer-file-name)))
+                 nil "setsid" "openscad" (buffer-file-name)))
+
 (after! scad-mode
   (define-key scad-mode-map (kbd "C-c C-p")
     'open-in-openscad))
+
+(defun scad-cheatshet ()
+  "Open the SCAD Cheatsheet in a web browser"
+  (interactive)
+  (browse-url  "https://openscad.org/cheatsheet/"))
