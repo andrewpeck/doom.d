404a6b3662bdc606125cecb14bd73797a85f6963
Andrew Peck <andrew.peck@cern.ch>
2022-12-22 22:05:25 -0500

    use zstd for dired compression

--- config-random.el
+++ config-random.el
@@ -139,10 +139,17 @@ This command does not push text to `kill-ring'."
   (setq global-hl-todo-mode t))
 
 ;; better dired soring
-(after! dired
+(after! dired-x
   (setq dired-omit-extensions (remove ".bin" dired-omit-extensions))
   (setq dired-omit-extensions (remove ".bit" dired-omit-extensions))
   (setq dired-listing-switches "-a1vBhl  --group-directories-first"))
+
+(after! diredfl
+  (add-to-list 'diredfl-compressed-extensions ".zst"))
+
+(after! dired-aux
+  (setq dired-compress-file-default-suffix ".zst")
+  (setq dired-compress-directory-default-suffix ".tar.zst"))
 
 ;; add a margin while scrolling
 (setq scroll-margin 30)
