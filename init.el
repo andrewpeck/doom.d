2518f1ee8b906ff01e09667f524a844c48fe2ddc
Andrew Peck <andrew.peck@cern.ch>
2022-12-30 23:02:28 -0500

    re-enable doom dash

--- init.el
+++ init.el
@@ -29,7 +29,7 @@
     :ui
         doom                ; what makes DOOM look the way it does
       ;;deft                ; notational velocity for Emacs
-      ;;doom-dashboard      ; a nifty splash screen for Emacs
+      doom-dashboard        ; a nifty splash screen for Emacs
       ;;doom-quit           ; DOOM quit-message prompts when you quit Emacs
       hl-todo             ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
       ;;hydra               ; This module adds hydra to Doom Emacs
