a9d07949497a14555c40053b602b0f16c1156dc3
Andrew Peck <andrew.peck@cern.ch>
2022-12-30 20:27:55 -0500

    modeline cleanup

--- config.el
+++ config.el
@@ -48,6 +48,64 @@
 (delight 'markdown-mode            "Md"           "markdown")
 (delight 'poly-markdown-mode       " Pm"          "poly-markdown")
 
+(defun simple-mode-line-render (left right)
+  "Return a string of `window-width' length.
+Containing LEFT, and RIGHT aligned respectively."
+  (let ((available-width
+         (- (window-total-width)
+            (+ (length (format-mode-line left))
+               (length (format-mode-line right))))))
+    (append left
+            (list (format (format "%%%ds" available-width) ""))
+            right)))
+
+(setq mode-line-buffer-identification
+      (propertized-buffer-identification "%b"))
+
+(setq flycheck-mode-line '(:eval (replace-regexp-in-string
+                                  "FlyC" ""
+                                  (flycheck-mode-line-status-text))))
+
+'(:eval (propertize (concat "\t[" mode-name "] %l:%i\t") 'face '(:foreground "black" :height 0.9 :weight normal)
+                    'help-echo (buffer-file-name)))
+
+(defun flycheck-mode-line-status-text (&optional status)
+  "Get a text describing STATUS for use in the mode line.
+
+STATUS defaults to `flycheck-last-status-change' if omitted or
+nil."
+  (pcase (or status flycheck-last-status-change)
+
+    (`not-checked "∄")
+    (`no-checker "⛌")
+    (`running "⟳")
+    (`errored "‼")
+    (`finished
+     (let-alist (flycheck-count-errors flycheck-current-errors)
+       (if (or .error .warning)
+           (concat
+            "" (propertize (format "%s" (or .error 0) ) 'face '(:foreground "red"))
+            "|" (propertize (format "%s" (or .warning 0)) 'face '(:foreground "orange")))
+         " ✓")))
+    (`interrupted ".")
+    (`suspicious "?")))
+
+(setq-default mode-line-format
+              '((:eval (simple-mode-line-render
+                        ;; Left.
+                        `("%e "
+                          mode-line-mule-info
+                          evil-mode-line-tag
+                          mode-line-modified " "
+                          mode-line-buffer-identification)
+
+                        ;; Right.
+                        `("L%l:C%c:%p"
+                          (vc-mode vc-mode)
+                          mode-line-frame-identification
+                          mode-line-modes
+                          flycheck-mode-line
+                          " ")))))
 
 ;;------------------------------------------------------------------------------
 ;; to file
