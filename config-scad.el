ab8e4771f07a5ced5d2dabaecad52bc68a8505f0
Andrew Peck <andrew.peck@cern.ch>
2022-12-31 00:07:36 -0500

    defer loading

--- config-scad.el
+++ config-scad.el
@@ -4,29 +4,31 @@
 ;; OpenSCAD Checker
 ;;------------------------------------------------------------------------------
 
-(flycheck-define-checker openscad
-  "Runs openscad"
-  :command ("openscad"
-            (eval (concat "-o" (flycheck-temp-dir-system) "/scad-tmp.png"))
-            source-inplace)
-  :error-patterns
-  ;; different versions of scad produce slightly different error messages... uhg
-  ((error line-start "ERROR:" (message) " " (file-name)  ", line " line line-end)
-   (error line-start "ERROR:" (message) "\"" (file-name) "\", line " line ": syntax error" line-end))
-  :modes (scad-mode))
-(add-to-list 'flycheck-checkers 'openscad)
-
-(defun open-in-openscad ()
-  "Open the current buffer in openscad"
-  (interactive)
-  (start-process (format "*scad-%s*" (file-name-base (buffer-file-name)))
-                 nil "setsid" "openscad" (buffer-file-name)))
+(after! flycheck
+  (flycheck-define-checker openscad
+    "Runs openscad"
+    :command ("openscad"
+              (eval (concat "-o" (flycheck-temp-dir-system) "/scad-tmp.png"))
+              source-inplace)
+    :error-patterns
+    ;; different versions of scad produce slightly different error messages... uhg
+    ((error line-start "ERROR:" (message) " " (file-name)  ", line " line line-end)
+     (error line-start "ERROR:" (message) "\"" (file-name) "\", line " line ": syntax error" line-end))
+    :modes (scad-mode))
+  (add-to-list 'flycheck-checkers 'openscad))
 
 (after! scad-mode
-  (define-key scad-mode-map (kbd "C-c C-p")
-    'open-in-openscad))
 
-(defun scad-cheatshet ()
-  "Open the SCAD Cheatsheet in a web browser"
-  (interactive)
-  (browse-url  "https://openscad.org/cheatsheet/"))
+  (defun open-in-openscad ()
+    "Open the current buffer in openscad"
+    (interactive)
+    (start-process (format "*scad-%s*" (file-name-base (buffer-file-name)))
+                   nil "setsid" "openscad" (buffer-file-name)))
+
+  (define-key scad-mode-map (kbd "C-c C-p")
+    'open-in-openscad)
+
+  (defun scad-cheatshet ()
+    "Open the SCAD Cheatsheet in a web browser"
+    (interactive)
+    (browse-url  "https://openscad.org/cheatsheet/")))
--- config-tex.el
+++ config-tex.el
@@ -56,33 +56,33 @@
 ;; TODO: check for common things at end of line:
 ;; c.f. e.g. i.e.
 
-(defun ap/line-fill-paragraph (&optional P)
-  "When called with prefix argument call `fill-paragraph'.
+(after! evil
+  (defun ap/line-fill-paragraph (&optional P)
+    "When called with prefix argument call `fill-paragraph'.
    Otherwise split the current paragraph into one sentence per line."
-  (interactive "P")
-  (if (not P)
-      (save-excursion
-        (let ((fill-column 12345678)) ;; relies on dynamic binding
-          (fill-paragraph) ;; this will not work correctly if the paragraph is
-          ;; longer than 12345678 characters (in which case the
-          ;; file must be at least 12MB long. This is unlikely.)
-          (let ((end (save-excursion
-                       (forward-paragraph 1)
-                       (backward-sentence)
-                       (point-marker))))  ;; remember where to stop
-            (beginning-of-line)
-            (while (progn (forward-sentence)
-                          (<= (point) (marker-position end)))
-              (just-one-space) ;; leaves only one space, point is after it
-              (delete-char -1) ;; delete the space
-              (newline)        ;; and insert a newline
-              (evil-indent-line (line-beginning-position) (line-end-position))))))
+    (interactive "P")
+    (if (not P)
+        (save-excursion
+          (let ((fill-column 12345678)) ;; relies on dynamic binding
+            (fill-paragraph) ;; this will not work correctly if the paragraph is
+            ;; longer than 12345678 characters (in which case the
+            ;; file must be at least 12MB long. This is unlikely.)
+            (let ((end (save-excursion
+                         (forward-paragraph 1)
+                         (backward-sentence)
+                         (point-marker))))  ;; remember where to stop
+              (beginning-of-line)
+              (while (progn (forward-sentence)
+                            (<= (point) (marker-position end)))
+                (just-one-space) ;; leaves only one space, point is after it
+                (delete-char -1) ;; delete the space
+                (newline)        ;; and insert a newline
+                (evil-indent-line (line-beginning-position) (line-end-position))))))
 
-    ;; otherwise do ordinary fill paragraph
-    (fill-paragraph P)))
-
-(evil-define-key 'normal latex-mode-map (kbd "M-q") #'ap/line-fill-paragraph)
-(evil-define-key 'normal latex-mode-map (kbd "M-q") nil)
+      ;; otherwise do ordinary fill paragraph
+      (fill-paragraph P)))
+  (evil-define-key 'normal latex-mode-map (kbd "M-q") #'ap/line-fill-paragraph)
+  (evil-define-key 'normal latex-mode-map (kbd "M-q") nil))
 
 ;; TeX
 ;;------------------------------------------------------------------------------
@@ -94,24 +94,24 @@
   (add-hook 'LaTeX-mode-hook (lambda () (reftex-mode 1)))
   (add-hook 'reftex-toc-mode-hook
             (lambda ()
-              (define-key reftex-toc-mode-map (kbd "<return>") 'reftex-toc-goto-line))))
+              (define-key reftex-toc-mode-map (kbd "<return>") 'reftex-toc-goto-line)))
 
-;; Electric Space
-;;------------------------------------------------------------------------------
+  ;; Electric Space
+  ;;------------------------------------------------------------------------------
 
-(defun electric-space () ; Trying to get Emacs to do semantic linefeeds
-  (interactive)
-  (if (looking-back (sentence-end) nil)
-      (insert "\n")
-    (self-insert-command 1)))
+  (defun electric-space ()              ; Trying to get Emacs to do semantic linefeeds
+    (interactive)
+    (if (looking-back (sentence-end) nil)
+        (insert "\n")
+      (self-insert-command 1)))
 
-(defvar electric-space-on-p nil)
+  (defvar electric-space-on-p nil)
 
-(defun toggle-electric-space ()
-  (interactive)
-  (global-set-key
-   " "
-   (if (setq electric-space-on-p
-             (not electric-space-on-p))
-       'electric-space
-     'self-insert-command)))
+  (defun toggle-electric-space ()
+    (interactive)
+    (global-set-key
+     " "
+     (if (setq electric-space-on-p
+               (not electric-space-on-p))
+         'electric-space
+       'self-insert-command))))
