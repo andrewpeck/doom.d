24c616ac4bd56543ece4ab695a2e63977f13b9c5
Andrew Peck <andrew.peck@cern.ch>
2023-02-28 19:24:39 -0500

    add magit submodule update all cmd

--- config-git.el
+++ config-git.el
@@ -11,7 +11,21 @@
   (add-hook! magit-diff-mode-hook
     (setq-local truncate-lines nil))
   (setq magit-repository-directories '(("~/work" . 1)))
-  (setq-default magit-diff-refine-hunk 'all))
+  (setq-default magit-diff-refine-hunk 'all)
+
+  ;; from: https://www.reddit.com/r/emacs/comments/1187mmq/can_magit_update_synce_and_init_all_submodules/
+  (unless (condition-case nil
+              (transient-get-suffix 'magit-submodule "U")
+            (error nil)) ;; catch error if suffix does not exist, and return nil to (unless)
+    (transient-define-suffix magit-submodule-update-all (args)
+      "Update all submodules"
+      :class 'magit--git-submodule-suffix
+      :description "Update all modules    git submodule update --init [--recursive]"
+      (interactive (list (magit-submodule-arguments "--recursive")))
+      (magit-with-toplevel
+        (magit-run-git-async "submodule" "update" "--init" args)))
+    (transient-append-suffix 'magit-submodule '(2 -1) ;; add as last entry in the 3rd section
+      '("U" magit-submodule-update-all))))
 
 (after! browse-at-remote
   (add-to-list 'browse-at-remote-remote-type-regexps
