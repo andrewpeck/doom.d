a0c37be38e34bbf93863ad2ae1136592824ff349
Andrew Peck <andrew.peck@cern.ch>
2023-04-30 13:39:55 -0400

    update to use gpt-3.5-turbo

--- packages/gpt-emacs/gpt.el
+++ packages/gpt-emacs/gpt.el
@@ -34,10 +34,11 @@
 
 (defvar gpt-api-key "sk-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" "A key for openai.")
 (defvar gpt-cost-per-token (/ 2.0 1000.0) "Cost for single GPT token.")
-(defvar gpt-text-model "text-davinci-003" "Model used by gpt.")
+(defvar gpt-text-model "gpt-3.5-turbo" "Model used by gpt.")
 (defvar gpt-max-tokens 256 "Max number of tokens.")
 (defvar gpt-temperature 0.7 "GPT3 Temperature.")
 (defvar gpt-top-p 1 "GPT3 Top_p. Distribution of probably of common tokens.")
+;; (defvar gpt-api-role "You are a helpful coding and writing assistant." "GPT3 Role")
 
 (defun gpt-api-call (prompt)
   "Posts a GPT api request based on the PROMPT.
@@ -45,17 +46,23 @@ Returns a list decoded from the JSON reponse."
 
   (let* ((json-array-type 'list)
          (post-body
-          (json-encode `(("model" . ,gpt-text-model)
-                         ("max_tokens" . ,gpt-max-tokens)
-                         ("temperature" . ,gpt-temperature)
-                         ("top_p" . ,gpt-top-p)
-                         ("prompt" . ,prompt)
-                         ("frequency_penalty" . 0)
-                         ("presence_penalty" . 0))))
+          (json-encode (list
+                        (cons "model" gpt-text-model)
+                        (cons "max_tokens" gpt-max-tokens)
+                        (cons "temperature" gpt-temperature)
+                        `("messages" .
+                          ,(list `(("role" . "user")
+                                   ("content" . ,prompt))
+                                 ;; `(("role" . "system")
+                                 ;;   ("content" . ,gpt-api-role))
+                                 ))
+                        (cons "top_p" gpt-top-p)
+                        (cons "frequency_penalty" 0)
+                        (cons "presence_penalty" 0))))
          (post-headers `(("Content-Type" . "application/json")
                          ("Authorization" . ,(concat  "Bearer " gpt-api-key))))
          (post (plz 'post
-                 "https://api.openai.com/v1/completions"
+                 "https://api.openai.com/v1/chat/completions"
                  :headers post-headers :body post-body
                  :as #'json-read :then 'sync))) post))
 
@@ -72,6 +79,7 @@ cost in tokens. The resulting text is then inserted into the
 buffer, and the usage cost is displayed in a message."
   (interactive)
   (let ((selection ""))
+
     (when (region-active-p)
       (setq selection (buffer-substring-no-properties
                        (region-beginning)
@@ -85,7 +93,8 @@ buffer, and the usage cost is displayed in a message."
       (when (not (string-empty-p prompt))
         (let* ((prompt-text (format "%s .\\n\\n%s" prompt selection))
                (rx-json (gpt-api-call prompt-text))
-               (text (string-trim (cdaadr (assoc 'choices rx-json))))
+               (text (string-trim
+                      (cdr (assoc 'content (cdaadr (assoc 'choices rx-json))))))
                (usage (cdr (assoc 'total_tokens (assoc 'usage rx-json)))))
 
           (print selection)
