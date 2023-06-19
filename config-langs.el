d06cecbd7ea6ac5ccc1a5f7cf81237e62efe44e5
Andrew Peck <andrew.peck@cern.ch>
2023-06-18 22:30:47 -0400

    defer flycheck config

--- config-langs.el
+++ config-langs.el
@@ -17,27 +17,28 @@
   (setq-local  comment-start "# "))
 
 ;; redefine the awk checker to have no-ext enabled on the linter "--lint=no-ext"
-(flycheck-define-checker awk-gawk
-  "GNU awk's built-in --lint checker."
-  :command ("gawk"
-            ;; Avoid code execution.  See https://github.com/w0rp/ale/pull/1411
-            "--source" "'BEGIN{exit} END{exit 1}'"
-            "-f" source
-            "--lint=no-ext"
-            "/dev/null")
-  :standard-input nil
-  :error-patterns
-  ((warning line-start
-            "gawk: "
-            (file-name) ":" line ":" (optional column ":")
-            (message (one-or-more not-newline)
-                     (optional "\n"
-                               (one-or-more not-newline)
-                               " ^ "
-                               (one-or-more not-newline)))
-            line-end))
-  :error-filter flycheck-awk-gawk-error-filter
-  :modes awk-mode)
+(after! flycheck
+  (flycheck-define-checker awk-gawk
+    "GNU awk's built-in --lint checker."
+    :command ("gawk"
+              ;; Avoid code execution.  See https://github.com/w0rp/ale/pull/1411
+              "--source" "'BEGIN{exit} END{exit 1}'"
+              "-f" source
+              "--lint=no-ext"
+              "/dev/null")
+    :standard-input nil
+    :error-patterns
+    ((warning line-start
+              "gawk: "
+              (file-name) ":" line ":" (optional column ":")
+              (message (one-or-more not-newline)
+                       (optional "\n"
+                                 (one-or-more not-newline)
+                                 " ^ "
+                                 (one-or-more not-newline)))
+              line-end))
+    :error-filter flycheck-awk-gawk-error-filter
+    :modes awk-mode))
 
 ;;------------------------------------------------------------------------------
 ;; Elisp
