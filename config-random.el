a7a6b41a6718ac79d9b4595b22beec66a803d1c1
Andrew Peck <peckandrew@gmail.com>
2023-08-21 20:04:13 -0700

    automatically create emacs-backups if it does not exist

--- config-random.el
+++ config-random.el
@@ -95,9 +95,15 @@
       :n [mouse-9] #'next-buffer)
 
 ;; Backups
+;; FIXME: after?
 (require 'backup-each-save)
-(setq backup-each-save-mirror-location "~/emacs-backups")
+(setq backup-each-save-mirror-location (expand-file-name "~/emacs-backups"))
+(when (not (f-directory-p backup-each-save-mirror-location))
+  (make-directory backup-each-save-mirror-location))
+
 (add-hook 'after-save-hook 'backup-each-save)
+
+
 
 ;;
 (remove-hook 'doom-first-buffer-hook #'smartparens-global-mode)
