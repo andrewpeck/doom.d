a3c74d1093f31c5b7f9b5f476f1af7dbd78b3d43
Andrew Peck <peckandrew@gmail.com>
2023-09-14 21:44:48 -0700

    eglot python config

--- config-eglot.el
+++ config-eglot.el
@@ -3,6 +3,15 @@
 ;;------------------------------------------------------------------------------
 
 (after! eglot
+                                        ; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=61373
+  ;; pyright generates html :(
+  ;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=61373
+  ;; (add-hook 'python-mode-hook
+  ;;           (lambda ()
+  ;;             ))
+  (setq eglot-prefer-plaintext t)
+  (setq eldoc-echo-area-prefer-doc-buffer t
+        eldoc-echo-area-use-multiline-p nil)
 
   (dolist (provider '(:hoverProvider :documentHighlightProvider))
     (add-to-list 'eglot-ignored-server-capabilities provider))
