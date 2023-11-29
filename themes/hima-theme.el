b7f9b32a31a0dda5d7a41f92537753ea31a0d06d
Andrew Peck <peckandrew@gmail.com>
2023-11-28 17:17:19 -0800

    reformat

--- themes/hima-theme.el
+++ themes/hima-theme.el
@@ -60,150 +60,173 @@
     "Simple gray color for use outside comments"
     :group 'hima)
 
-  (custom-theme-set-faces 'hima
-    ;; Basic items
-    `(default ,default-layer)
-    `(nobreak-space ,default-layer)
-    `(underline ,underline-layer)
-    `(cursor ((t (:background ,black))))
-    `(fringe ((t (:background ,white :foreground ,light-gray))))
-    `(highlight ((t (:background ,light-blue))))
-    `(region ((t (:background ,lighter-blue :foreground ,black))))
-    `(tooltip ((t (:background ,light-black :foreground ,medium-gray))))
-    `(vertical-border ((t (:background ,lighter-gray :foreground ,lighter-gray))))
-    `(widget-button ((t (:foreground ,dark-blue))))
+  (custom-theme-set-faces
+   'hima
+   ;; Basic items
+   `(default ,default-layer)
+   `(nobreak-space ,default-layer)
+   `(underline ,underline-layer)
+   `(cursor ((t (:background ,black))))
+   `(fringe ((t (:background ,white :foreground ,light-gray))))
+   `(highlight ((t (:background ,light-blue))))
+   `(region ((t (:background ,lighter-blue :foreground ,black))))
+   `(tooltip ((t (:background ,light-black :foreground ,medium-gray))))
+   `(vertical-border ((t (:background ,lighter-gray :foreground ,lighter-gray))))
+   `(widget-button ((t (:foreground ,dark-blue))))
 
-    ;; Gui items
-    `(header-line ((t (:foreground ,black :box (:line-width 1 :color ,lighter-gray :style nil))))) ; Works best for topsy-mode
-    `(mode-line-inactive ((t (:background ,light-gray))))
-    `(mode-line-buffer-id ,bold-layer)
-    `(mode-line ((t (:background ,green-blue))))
-    `(minibuffer-prompt ((t (:foreground ,blue))))
+   `(org-table ,default-layer)
 
-    ;; Basic font lock entries
-    `(font-lock-keyword-face ,default-layer)
-    `(font-lock-warning-face ,default-layer)
-    `(font-lock-function-name-face ,definition-layer)
-    `(font-lock-type-face ,default-layer)
-    `(font-lock-preprocessor-face ,default-layer)
-    `(font-lock-builtin-face ,default-layer)
-    `(font-lock-variable-name-face ,default-layer)
-    `(font-lock-string-face ,string-layer)
-    `(font-lock-constant-face ,default-layer)
-    `(font-lock-doc-face ,commentary-layer)
-    `(font-lock-comment-face ,commentary-layer)
+   ;; Flycheck
+   `(flycheck-error   ((t (:underline (:color ,light-red   :style wave)))))
+   `(flycheck-info    ((t (:underline (:color ,light-green :style wave)))))
+   `(flycheck-warning ((t (:underline (:color ,barely-orange :style wave)))))
 
-    ;; show-paren
-    `(show-paren-mismatch ,red-light-red-layer)
-    `(show-paren-match ,tooltip-selected-layer)
+   ;; Gui items
+   `(header-line ((t (:foreground ,black :box (:line-width 1 :color ,lighter-gray :style nil))))) ; Works best for topsy-mode
+   `(mode-line-inactive ((t (:background ,light-gray))))
+   `(mode-line-buffer-id ,bold-layer)
+   `(mode-line ((t (:background ,light-green))))
+   `(minibuffer-prompt ((t (:foreground ,blue))))
 
-    ;; hl-line
-    `(hl-line ((t (:background ,lighter-gray))))
+   ;; Basic font lock entries
+   `(font-lock-warning-face ,default-layer)
+   `(font-lock-function-name-face ,definition-layer)
+   `(font-lock-keyword-face ,definition-layer)
+   `(font-lock-builtin-face ,definition-layer)
+   `(font-lock-type-face ,definition-layer)
+   `(font-lock-preprocessor-face ,default-layer)
+   `(font-lock-builtin-face ,default-layer)
+   `(font-lock-variable-name-face ,default-layer)
+   `(font-lock-string-face ,string-layer)
+   `(font-lock-constant-face ,default-layer)
+   `(font-lock-doc-face ,commentary-layer)
+   `(font-lock-comment-face ,commentary-layer)
 
-    ;; linum
-    `(linum ((t (:foreground ,black))))
+   ;; show-paren
+   `(show-paren-mismatch ,red-light-red-layer)
+   `(show-paren-match ,tooltip-selected-layer)
 
-    ;; company
-    `(company-tooltip ,tooltip-layer)
-    `(company-tooltip-annotation ,tooltip-layer)
-    `(company-tooltip-common ((t (:foreground ,black))))
-    `(company-tooltip-selection ,tooltip-selected-layer)
-    `(company-tooltip-annotation-selection ,tooltip-selected-layer)
-    `(company-tooltip-common-selection ,tooltip-selected-layer)
-    `(company-preview ((t (:foreground ,dark-gray :background ,(face-attribute 'default :background)))))
-    `(company-preview-common ((t (:foreground ,dark-gray :background ,(face-attribute 'default :background)))))
-    `(company-scrollbar-bg ,all-black-layer)
-    `(company-scrollbar-fg ,all-white-layer)
+   ;; hl-line
+   `(hl-line ((t (:background ,lighter-gray))))
 
-    ;; flyspell
-    `(flyspell-duplicate ((t (:underline (:style wave :color ,dark-green)))))
-    `(flyspell-incorrect ((t (:underline (:style wave :color ,dark-orange)))))
+   ;; linum
+   `(linum ((t (:foreground ,black))))
 
-    ;; isearch
-    `(isearch ((t (:background ,light-blue :foreground ,dark-gray))))
+   ;; company
+   `(company-tooltip ,tooltip-layer)
+   `(company-tooltip-annotation ,tooltip-layer)
+   `(company-tooltip-common ((t (:foreground ,black))))
+   `(company-tooltip-selection ,tooltip-selected-layer)
+   `(company-tooltip-annotation-selection ,tooltip-selected-layer)
+   `(company-tooltip-common-selection ,tooltip-selected-layer)
+   `(company-preview ((t (:foreground ,dark-gray :background ,(face-attribute 'default :background)))))
+   `(company-preview-common ((t (:foreground ,dark-gray :background ,(face-attribute 'default :background)))))
+   `(company-scrollbar-bg ,all-black-layer)
+   `(company-scrollbar-fg ,all-white-layer)
 
-    ;; vterm (need a lot more colors)
-    `(vterm-color-black ((t (:foreground "#444444" :background "#969694"))))
-    `(vterm-color-red ((t (:foreground "#9E0047" :background "#FF0072"))))
-    `(vterm-color-green ((t (:foreground "#718C00" :background "#8EB200"))))
-    `(vterm-color-yellow ((t (:foreground "#B25000" :background "#FF7200"))))
-    `(vterm-color-blue ((t (:foreground "#4271AE" :background "#5EA2F9"))))
-    `(vterm-color-magenta ((t (:foreground "#8959A8" :background "#A56CCC"))))
-    `(vterm-color-cyan ((t (:foreground "#3E999F" :background "#51C8D1"))))
-    `(vterm-color-white ((t (:foreground "#F5F5F5" :background "#FFFFFF"))))
-    `(vterm-color-underline ,underline-layer)
+   ;; flyspell
+   `(flyspell-duplicate ((t (:underline (:style wave :color ,dark-green)))))
+   `(flyspell-incorrect ((t (:underline (:style wave :color ,dark-orange)))))
 
-    `(term-color-black ((t (:foreground "#444444" :background "#969694"))))
-    `(term-color-red ((t (:foreground "#9E0047" :background "#FF0072"))))
-    `(term-color-green ((t (:foreground "#718C00" :background "#8EB200"))))
-    `(term-color-yellow ((t (:foreground "#B25000" :background "#FF7200"))))
-    `(term-color-blue ((t (:foreground "#4271AE" :background "#5EA2F9"))))
-    `(term-color-magenta ((t (:foreground "#8959A8" :background "#A56CCC"))))
-    `(term-color-cyan ((t (:foreground "#3E999F" :background "#51C8D1"))))
-    `(term-color-white ((t (:foreground "#F5F5F5" :background "#FFFFFF"))))
-    `(term-color-underline ,underline-layer)
+   ;; isearch
+   `(isearch ((t (:background ,light-blue :foreground ,dark-gray))))
 
-    ;; magit
-    `(diff-refine-removed ,reder-light-red-layer)
-    `(diff-refine-added ,greener-light-green-layer)
-    `(magit-diff-context ((t (:background ,white))))
-    `(magit-diff-context-highlight ((t (:background ,light-white))))
-    `(magit-diff-hunk-heading ((t (:background ,light-gray))))
-    `(magit-diff-hunk-heading-highlight ((t (:background ,medium-gray))))
-    `(magit-diff-removed ,red-light-red-layer)
-    `(magit-diff-added ,green-light-green-layer)
-    `(magit-diff-removed-highlight ,red-light-red-layer)
-    `(magit-diff-added-highlight ,green-light-green-layer)
-    `(magit-section-heading ((t (:foreground ,dark-blue))))
-    `(magit-section-highlight ((t (:foreground ,pink))))
+   ;; vterm (need a lot more colors)
+   `(vterm-color-black ((t (:foreground "#444444" :background "#969694"))))
+   `(vterm-color-red ((t (:foreground "#9E0047" :background "#FF0072"))))
+   `(vterm-color-green ((t (:foreground "#718C00" :background "#8EB200"))))
+   `(vterm-color-yellow ((t (:foreground "#B25000" :background "#FF7200"))))
+   `(vterm-color-blue ((t (:foreground "#4271AE" :background "#5EA2F9"))))
+   `(vterm-color-magenta ((t (:foreground "#8959A8" :background "#A56CCC"))))
+   `(vterm-color-cyan ((t (:foreground "#3E999F" :background "#51C8D1"))))
+   `(vterm-color-white ((t (:foreground "#F5F5F5" :background "#FFFFFF"))))
+   `(vterm-color-underline ,underline-layer)
 
-    ;; tree-sitter
-    `(tree-sitter-hl-face:comment ,commentary-layer)
-    `(tree-sitter-hl-face:doc ((t (:foreground ,dark-green))))
-    `(tree-sitter-hl-face:string ,string-layer)
-    `(tree-sitter-hl-face:punctuation.delimiter ,default-layer)
-    `(tree-sitter-hl-face:keyword ,default-layer)
-    `(tree-sitter-hl-face:property ,default-layer)
-    `(tree-sitter-hl-face:method.call ,default-layer)
-    `(tree-sitter-hl-face:function.call ,default-layer)
-    `(tree-sitter-hl-face:function.method ,definition-layer)
-    `(tree-sitter-hl-face:constructor ,definition-layer)
+   `(term-color-black ((t (:foreground "#444444" :background "#969694"))))
+   `(term-color-red ((t (:foreground "#9E0047" :background "#FF0072"))))
+   `(term-color-green ((t (:foreground "#718C00" :background "#8EB200"))))
+   `(term-color-yellow ((t (:foreground "#B25000" :background "#FF7200"))))
+   `(term-color-blue ((t (:foreground "#4271AE" :background "#5EA2F9"))))
+   `(term-color-magenta ((t (:foreground "#8959A8" :background "#A56CCC"))))
+   `(term-color-cyan ((t (:foreground "#3E999F" :background "#51C8D1"))))
+   `(term-color-white ((t (:foreground "#F5F5F5" :background "#FFFFFF"))))
+   `(term-color-underline ,underline-layer)
 
-    ;; fancy-narrow
-    `(fancy-narrow-blocked-face ,almost-invisible-layer)
+   ;; magit
+   `(diff-refine-removed ,reder-light-red-layer)
+   `(diff-refine-added ((t (:background ,almost-light-green))))
+   `(diff-refine-changed ((t (:background ,lighter-orange))))
+   `(diff-hl-delete ,reder-light-red-layer)
+   `(diff-hl-insert ((t (:background ,dark-green))))
+   `(diff-hl-change ((t (:background ,lighter-orange))))
 
-    ;; dired
-    `(dired-directory ((t (:weight bold :foreground ,black))))
-    `(dired-ignored ,disabled-layer)
+   `(magit-diff-context ((t (:background ,white))))
+   `(magit-diff-context-highlight ((t (:background ,light-white))))
+   `(magit-diff-hunk-heading ((t (:background ,light-gray))))
+   `(magit-diff-hunk-heading-highlight ((t (:background ,medium-gray))))
+   `(magit-diff-removed ,red-light-red-layer)
+   `(magit-diff-added ,green-light-green-layer)
+   `(magit-diff-removed-highlight ,red-light-red-layer)
+   `(magit-diff-added-highlight ,green-light-green-layer)
+   `(magit-section-heading ((t (:foreground ,dark-blue))))
+   `(magit-section-highlight ((t (:foreground ,pink))))
 
-    ;; shr
-    `(shr-link ,underline-layer)
+   ;; tree-sitter
+   `(tree-sitter-hl-face:comment ,commentary-layer)
+   `(tree-sitter-hl-face:doc ((t (:foreground ,dark-green))))
+   `(tree-sitter-hl-face:string ,string-layer)
+   `(tree-sitter-hl-face:punctuation.delimiter ,default-layer)
+   `(tree-sitter-hl-face:keyword ,default-layer)
+   `(tree-sitter-hl-face:property ((t (:foreground ,dark-blue))))
+   `(tree-sitter-hl-face:method.call ,definition-layer)
+   `(tree-sitter-hl-face:function.call ,default-layer)
+   `(tree-sitter-hl-face:function.method ,definition-layer)
+   `(tree-sitter-hl-face:constructor ,definition-layer)
 
-    ;; variable-pitch
-    `(variable-pitch ((t nil)))
+   ;; fancy-narrow
+   `(fancy-narrow-blocked-face ,almost-invisible-layer)
 
-    ;; strips-mode
-    `(stripe-highlight ((t (:background ,lighter-blue))))
-    `(stripe-hl-line ((t (:background ,barely-orange))))
+   ;; dired
+   `(dired-directory ((t (:weight bold :foreground ,black))))
+   `(diredfl-dir-name ((t (:weight bold :foreground ,black))))
+   `(diredfl-dir-heading ((t (:weight bold :foreground ,black))))
+   `(diredfl-dir-priv ,default-layer)
+   `(diredfl-read-priv ,default-layer)
+   `(diredfl-write-priv ,default-layer)
+   `(diredfl-exec-priv ,default-layer)
+   `(diredfl-no-priv ,default-layer)
+   `(diredfl-number ,definition-layer)
+   `(diredfl-date-time ,commentary-layer)
+   `(dired-ignored ,disabled-layer)
 
-    ;; eldoc
-    `(eldoc-highlight-function-argument ((t (:foreground ,dark-blue))))
+   ;; shr
+   `(shr-link ,underline-layer)
 
-    ;; denote
-    `(denote-faces-date ((t :foreground ,medium-gray)))
-    `(denote-faces-time ((t :foreground ,medium-gray)))
-    `(denote-faces-title ,default-layer)
-    `(denote-faces-extension ,commentary-layer)
-    `(denote-faces-keywords ((t :foreground ,medium-gray)))
+   ;; variable-pitch
+   `(variable-pitch ((t nil)))
 
-    ;; diff-hl
-    `(diff-hl-change ((t (:background ,barely-orange))))
-    `(diff-hl-insert ((t (:background ,light-green))))
-    `(diff-hl-delete ((t (:background ,light-red))))
+   ;; strips-mode
+   `(stripe-highlight ((t (:background ,lighter-blue))))
+   `(stripe-hl-line ((t (:background ,barely-orange))))
 
-    ;; keycast
-    `(keycast-key ((t (:background ,light-blue))))
-    ))
+   ;; eldoc
+   `(eldoc-highlight-function-argument ((t (:foreground ,dark-blue))))
+
+   ;; denote
+   `(denote-faces-date ((t :foreground ,medium-gray)))
+   `(denote-faces-time ((t :foreground ,medium-gray)))
+   `(denote-faces-title ,default-layer)
+   `(denote-faces-extension ,commentary-layer)
+   `(denote-faces-keywords ((t :foreground ,medium-gray)))
+
+   ;; diff-hl
+   `(diff-hl-change ((t (:background ,barely-orange))))
+   `(diff-hl-insert ((t (:background ,light-green))))
+   `(diff-hl-delete ((t (:background ,light-red))))
+
+   ;; keycast
+   `(keycast-key ((t (:background ,light-blue))))
+   ))
 
 (provide-theme 'hima)
 ;;; hima-theme.el ends here
