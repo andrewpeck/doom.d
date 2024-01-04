6cd54305d800b97cd952e7d4aefb36887bcb4834
Andrew Peck <peckandrew@gmail.com>
2024-01-04 15:31:41 -0800

    enable evil mode in all text buffers

--- config.el
+++ config.el
@@ -49,6 +49,9 @@
 
 (setq doom-scratch-dir doom-user-dir)
 
+;; make sure all text-mode buffers (e.g. fundamental mode, messages, etc) use evil
+(add-hook! 'text-mode-hook #'turn-on-evil-mode)
+
 ;; start:sort
 (load!! "config-org")
 (load!! "config-align")
