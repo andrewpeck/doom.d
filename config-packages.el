ece77aefc53dbfb9689b6cd0fc5265ac6d60dbdd
Andrew Peck <peckandrew@gmail.com>
2024-01-08 15:07:03 -0800

    consolidate dired config

--- config-packages.el
+++ config-packages.el
@@ -137,12 +137,29 @@
 ;;------------------------------------------------------------------------------
 ;; Dired
 ;;------------------------------------------------------------------------------
+;;
+;; Add git commit into into dired
+;; (add-hook 'dired-after-readin-hook
+;; 'dired-git-info-auto-enable)
+
+(after! diredfl
+  (add-to-list 'diredfl-compressed-extensions ".zst"))
+
+(after! dired-aux
+  (setq dired-compress-file-default-suffix ".zst")
+  (setq dired-compress-directory-default-suffix ".tar.zst"))
 
 (use-package! dired-x
 
   :after dired
 
   :config
+
+  (setq dired-omit-extensions (remove ".bin" dired-omit-extensions))
+  (setq dired-omit-extensions (remove ".bit" dired-omit-extensions))
+  ;; better dired soring
+  (setq dired-listing-switches "-a1vBhl  --group-directories-first")
+
   (setq dired-omit-files
         (concat dired-omit-files
                 "\\|vivado.*\\.jou\\'"
