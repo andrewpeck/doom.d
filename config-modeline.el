b0c3a55c661a908fd2d94b2345cfb164c0b2f0c3
Andrew Peck <peckandrew@gmail.com>
2024-02-21 10:09:26 -0800

    cleaner modeline

--- config-modeline.el
+++ config-modeline.el
@@ -20,18 +20,18 @@ STATUS defaults to `flycheck-last-status-change' if omitted or
 nil."
   (concat
    (pcase (or status flycheck-last-status-change)
-     (`not-checked "  ")
-     (`no-checker "  ")         ;✗
-     (`running "  ")
-     (`errored "  ")           ; ‼
-     (`interrupted " . ")        ;✓
-     (`suspicious " ? ")
+     (`not-checked "  ")
+     (`no-checker "  ")                ;✗
+     (`running "  ")
+     (`errored "  ")                   ; ‼
+     (`interrupted " . ")               ;✓
+     (`suspicious " ? ")
      (`finished (let-alist (flycheck-count-errors flycheck-current-errors)
                   (if (or .error .warning)
                       (concat
-                       "" (propertize (format "%s" (or .error 0) ) 'face '(:inherit error))
-                       "|" (propertize (format "%s" (or .warning 0)) 'face '(:inherit warning)))
-                    "  ")))) " "))
+                       (propertize (format "%s" (or .error "0") ) 'face '(:inherit error))
+                       "﹢" (propertize (format "%s" (or .warning "0")) 'face '(:inherit warning)))
+                    "  "))))))
 
 ;; https://emacs.stackexchange.com/questions/10955/customize-vc-mode-appearance-in-mode-line
 
@@ -63,20 +63,19 @@ nil."
 
                 ;; Right.
                 (list (if (or defining-kbd-macro executing-kbd-macro)
-                          (concat "MACRO(" (char-to-string evil-this-macro) ") ¦ ") "")
+                          (concat "MACRO(" (char-to-string evil-this-macro) ") ⁚ ") "")
 
                       "L%l⸱C%c⸱%p"
 
                       (if vc-mode
-                          (concat " ¦" vc-mode " ¦ ") " ¦ ")
+                          (concat " ⁚" vc-mode " ⁚ ") " ⁚ ")
 
                       (format "%s" (if (listp mode-name) (car mode-name) mode-name))
 
                       (replace-regexp-in-string "FlyC" ""
-                                                (concat " ¦ " 
+                                                (concat " ⁚ " 
                                                          (flycheck-mode-line-status-text)))
-
-                      " ")))))
+                      )))))
 
 (setq-default mode-line-format
               mode-line-format)
