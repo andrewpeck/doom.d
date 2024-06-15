10ffb77f8733c142a6fe658c8d0fe5aafb6fa663
Andrew Peck <peckandrew@gmail.com>
2024-06-15 12:02:38 -0700

    set tex capfs

--- config-packages.el
+++ config-packages.el
@@ -1909,7 +1909,13 @@ See URL `http://nagelfar.sourceforge.net/'."
                              vhdl-packages
                              vhdl-directives)))))
 
-
+  (add-hook! 'LaTeX-mode-hook
+    (defun hook/set-latex-capf-functions ()
+      (setq-local completion-at-point-functions
+                  (list 'lsp-completion-at-point
+                        'TeX--completion-at-point
+                        'yasnippet-capf
+                        'LaTeX--arguments-completion-at-point))))
 
   (add-hook! 'emacs-lisp-mode-hook
     (defun hook/set-elisp-capf-functions ()
