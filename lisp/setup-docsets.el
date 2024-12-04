8d0c0a066729894c57761c76a418c17886423183
Andrew Peck <peckandrew@gmail.com>
2024-12-04 13:29:21 -0800

    fix docsets

--- lisp/setup-docsets.el
+++ lisp/setup-docsets.el
@@ -5,9 +5,11 @@
 (use-package! dash-docs
   :defer-incrementally t
   :config
-  (defvar dash-docs-my-docsets
-    '("Tcl" "Python_3" "C" "C++" "Bash" "Clojure")
+
+  (defvar dash-docs-my-docsets nil
     "Docsets to be installed via `dash-docs-install-my-docsets'")
+
+  (setq dash-docs-my-docsets '("Tcl" "Python_3" "C" "C++" "Bash" "Clojure" "LaTeX"))
 
   (defun my/dash-docs-install-docsets ()
 
@@ -19,17 +21,15 @@
 
     (mkdir dash-docs-docsets-path t)
 
-    (let ((dash-docs-lib-path (locate-library "dash-docs"))
-          (dest dash-docs-docsets-path))
+    (dolist (docset dash-docs-my-docsets)
+      (when (not (file-exists-p (concat  dash-docs-docsets-path
+                                         (string-replace "_" " " docset) ".docset")))
 
-      (dolist (docset dash-docs-my-docsets)
-        (when (not (file-exists-p (concat  dash-docs-docsets-path
-                                           (string-replace "_" " " docset) ".docset")))
-          (message (concat "Installing " docset))
-          (async-start (lambda () (progn (load dash-docs-lib-path)
-                                         (require 'dash-docs)
-                                         (message (concat "Installing " docset))
-                                         (setq dash-docs-docsets-path dest)
-                                         (dash-docs-install-docset docset)
-                                         docset))
-                       (lambda (result) (message (format  "Docset %s installed" result)))))))))
+        (message (concat "Installing " docset))
+        (async-start `(lambda () (progn (load ,(locate-library "dash-docs"))
+                                        (require 'dash-docs)
+                                        (message (concat "Installing " ,docset))
+                                        (setq dash-docs-docsets-path ,dash-docs-docsets-path)
+                                        (dash-docs-install-docset ,docset)
+                                        ,docset))
+                     (lambda (result) (message (format  "Docset %s installed" result))))))))
