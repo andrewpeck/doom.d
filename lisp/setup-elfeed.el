b0333cfc1ffeff7fc238d68fcc24f1719de9ba9f
Andrew Peck <peckandrew@gmail.com>
2024-12-06 15:17:57 -0800

    elfeed updates

--- lisp/setup-elfeed.el
+++ lisp/setup-elfeed.el
@@ -9,10 +9,16 @@
   ;; Run `elfeed-update' every 8 hours
   (run-at-time nil (* 8 60 60) #'elfeed-update)
 
+  (evil-define-key 'normal elfeed-search-mode-map
+    (kbd "U") #'elfeed-update)
+
   (setq elfeed-feeds
         '("https://www.evalapply.org/index.xml"
           ;; "https://hackaday.com/blog/feed/"
+          "https://planet.emacslife.com/atom.xml"
+          "https://karl-voit.at/feeds/lazyblorg-all.atom_1.0.links-only.xml"
           "https://nullprogram.com/feed/"
+          "https://tracker.orgmode.org/bugs.rss"
           "https://bzg.fr/index.xml"
           "https://www.mattblaze.org/blog/rss20.xml"
           "https://jackrusher.com/feed.xml"
