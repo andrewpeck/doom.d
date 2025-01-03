9d53a7cbc3f388778ea46fd09931a35a8c135359
Andrew Peck <peckandrew@gmail.com>
2025-01-03 09:00:36 -0800

    org: use new org image preview api

--- lisp/setup-org.el
+++ lisp/setup-org.el
@@ -37,138 +37,50 @@
           (sequence "[ ](T)" "[-](S)" "[?](W)" "|" "[X](D)")
           (sequence "|" "OKAY(o)" "YES(y)" "NO(n)")))
 
-  ;; taken directly from org-yt
-  (defun org-display-user-inline-images (&optional _include-linked _refresh beg end)
-    "Like `org-display-inline-images' but for image data links.
-_INCLUDE-LINKED and _REFRESH are ignored.
-Restrict to region between BEG and END if both are non-nil.
-Image data links have a :image-data-fun parameter.
-\(See `org-link-set-parameters'.)
-The value of the :image-data-fun parameter is a function
-taking the PROTOCOL, the LINK, and the DESCRIPTION as arguments.
-If that function returns nil the link is not interpreted as image.
-Otherwise the return value is the image data string to be displayed.
+  (setq org-image-max-width 1280
+        org-image-align  'left
+        org-image-actual-width t)
 
-Note that only bracket links are allowed as image data links
-with one of the formats
- [[PROTOCOL:LINK]]
-or
- [[PROTOCOL:LINK][DESCRIPTION]]
-are recognized."
-    (interactive)
-    (when (and (called-interactively-p 'any)
-               (use-region-p))
-      (setq beg (region-beginning)
-            end (region-end)))
-    (when (display-graphic-p)
-      (org-with-wide-buffer
-       (goto-char (or beg (point-min)))
-       (when-let ((image-data-link-parameters
-		           (cl-loop for link-par-entry in org-link-parameters
-			                with fun
-			                when (setq fun (plist-get (cdr link-par-entry) :image-data-fun))
-			                collect (cons (car link-par-entry) fun)))
-		          (image-data-link-re (regexp-opt (mapcar 'car image-data-link-parameters)))
-		          (re (format "\\[\\[\\(%s\\):\\([^]]+\\)\\]\\(?:\\[\\([^]]+\\)\\]\\)?\\]"
-			                  image-data-link-re)))
-         (while (re-search-forward re end t)
-           (let* ((protocol (match-string-no-properties 1))
-		          (link (match-string-no-properties 2))
-		          (description (match-string-no-properties 3))
-		          (image-data-link (assoc-string protocol image-data-link-parameters))
-		          (el (save-excursion (goto-char (match-beginning 1)) (org-element-context)))
-		          image-data)
-             (when el
-               (setq image-data
-                     (or (let ((old (get-char-property-and-overlay
-                                     (org-element-property :begin el)
-                                     'org-image-overlay)))
-                           (and old
-                                (car-safe old)
-                                (overlay-get (cdr old) 'display)))
-		                 (funcall (cdr image-data-link) protocol link description)))
-               (when image-data
-                 (let ((ol (org-image-update-overlay image-data el t t)))
-                   (when (and ol description)
-                     (overlay-put ol 'after-string description)))))))))))
+  (defun +org-http-or-https-image-data-fn (ov link _elem type)
+    "Interpret LINK as an URL to an image file."
+    (when (and (image-supported-file-p link)
+               (not (eq org-display-remote-inline-images 'skip)))
+      (if-let (buf (url-retrieve-synchronously (concat type ":" link)))
+          (with-current-buffer buf
+            (goto-char (point-min))
+            (re-search-forward "\r?\n\r?\n" nil t)
+            (overlay-put ov 'display (create-image (buffer-substring-no-properties (point) (point-max)) nil t)))
+        (message "Download of image \"%s\" failed" link)
+        nil)))
 
+  (defun +org-http-or-https-image-data-fn (ov link _elem type)
+    "Interpret LINK as an URL to an image file."
+    (when (and (image-supported-file-p link)
+               (not (eq org-display-remote-inline-images 'skip)))
+      (let ((dest (concat temporary-file-directory
+                          (car (last (split-string link "/" t))))))
+        (when (not (file-exists-p dest))
+          (url-copy-file (concat type ":" link) dest t))
+        (org-link-preview-file ov dest link))))
 
-  (defun org-image-update-overlay (file link &optional data-p refresh)
-    "Create image overlay for FILE associtated with org-element LINK.
-If DATA-P is non-nil FILE is not a file name but a string with the image data.
-If REFRESH is non-nil don't download the file but refresh the image.
-See also `create-image'.
-This function is almost a duplicate of a part of `org-display-inline-images'."
-    (when (or data-p (file-exists-p file))
-      (let ((width
-             ;; Apply `org-image-actual-width' specifications.
-             (cond
-              ((eq org-image-actual-width t) nil)
-              ((listp org-image-actual-width)
-               (or
-                ;; First try to find a width among
-                ;; attributes associated to the paragraph
-                ;; containing link.
-                (let ((paragraph
-                       (let ((e link))
-                         (while (and (setq e (org-element-property
-                                              :parent e))
-                                     (not (eq (org-element-type e)
-                                              'paragraph))))
-                         e)))
-                  (when paragraph
-                    (save-excursion
-                      (goto-char (org-element-property :begin paragraph))
-                      (when
-                          (re-search-forward
-                           "^[ \t]*#\\+attr_.*?: +.*?:width +\\(\\S-+\\)"
-                           (org-element-property
-                            :post-affiliated paragraph)
-                           t)
-                        (string-to-number (match-string 1))))))
-                ;; Otherwise, fall-back to provided number.
-                (car org-image-actual-width)))
-              ((numberp org-image-actual-width)
-               org-image-actual-width)))
-            (old (get-char-property-and-overlay
-                  (org-element-property :begin link)
-                  'org-image-overlay)))
-        (if (and (car-safe old) refresh)
-            (image-refresh (overlay-get (cdr old) 'display))
-          (let ((image (create-image file
-                                     (and (image-type-available-p 'imagemagick)
-                                          width
-                                          'imagemagick)
-                                     data-p
-                                     :width width)))
-            (when image
-              (let* ((link
-                      ;; If inline image is the description
-                      ;; of another link, be sure to
-                      ;; consider the latter as the one to
-                      ;; apply the overlay on.
-                      (let ((parent
-                             (org-element-property :parent link)))
-                        (if (eq (org-element-type parent) 'link)
-                            parent
-                          link)))
-                     (ov (make-overlay
-                          (org-element-property :begin link)
-                          (progn
-                            (goto-char
-                             (org-element-property :end link))
-                            (skip-chars-backward " \t")
-                            (point)))))
-                (overlay-put ov 'display image)
-                (overlay-put ov 'face 'default)
-                (overlay-put ov 'org-image-overlay t)
-                (overlay-put
-                 ov 'modification-hooks
-                 (list 'org-display-inline-remove-overlay))
-                (push ov org-inline-image-overlays)
-                ov)))))))
+  (defun +org-http-image-data-fn (ov link elem)
+    "Interpret LINK as an URL to an image file."
+    (+org-http-or-https-image-data-fn ov link elem "http"))
 
-  (advice-add #'org-display-inline-images :after #'org-display-user-inline-images)
+  (defun +org-https-image-data-fn (ov link elem)
+    "Interpret LINK as an URL to an image file."
+    (+org-http-or-https-image-data-fn ov link elem "https"))
+
+  (defun +org-inline-image-data-fn (ov link _elem)
+    "Interpret LINK as base64-encoded image data."
+    (overlay-put ov 'display (create-image link nil t)))
+
+  (org-link-set-parameters "http"  :preview #'+org-http-image-data-fn)
+  (org-link-set-parameters "https" :preview #'+org-https-image-data-fn)
+  (org-link-set-parameters "img"   :preview #'+org-inline-image-data-fn)
+
+  (org-link-set-parameters
+   "docview" :preview #'org-link-docview-preview)
 
   (add-hook 'org-mode-hook #'org-excalidraw-initialize)
 
