c80ef97e0102d889a86df7d6f770f07d7203222c
Andrew Peck <peckandrew@gmail.com>
2025-01-19 12:16:56 -0800

    add org footnote edit function

--- lisp/setup-misc.el
+++ lisp/setup-misc.el
@@ -155,7 +155,8 @@ _h_ decrease width    _l_ increase width
      ((org-at-table-p) (lte-edit-table))
      ((org--link-at-point) (org-insert-link))
      ((org-in-src-block-p) (org-edit-src-code))
-     ((org-inside-LaTeX-fragment-p) (org-edit-latex-fragment))))
+     ((org-inside-LaTeX-fragment-p) (org-edit-latex-fragment))
+     ((org-footnote-at-reference-p) (org-edit-footnote-reference))))
 
   (define-key org-mode-map  (kbd  "C-c C-e") #'my/org-dwim-edit-at-point)
   (define-key markdown-mode-map  (kbd  "C-c C-e") #'lte-edit-table))
