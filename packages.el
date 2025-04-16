9a9b527352a7d3f60da1aad7619d5b029c5a8c14
Andrew Peck <peckandrew@gmail.com>
2025-04-16 11:18:27 -0700

    whitespace

--- packages.el
+++ packages.el
@@ -1,16 +1,18 @@
+;; -*- lexical-binding: t; -*-
+
 ;; Disabled packages
-(package! pyenv-mode :disable t)
-(package! pyenv      :disable t)
-(package! hl-line   :disable t)
-(package! diredfl   :disable t)
-(package! haml-mode  :disable t)
-(package! lsp       :disable t)
-(package! powerline :disable t)
-(package! popwin    :disable t)
-(package! ivy       :disable t)
-(package! swiper    :disable t)
-(package! org-yt    :disable t)
-(package! orgit-forge :disable t)
+(package! pyenv-mode   :disable t)
+(package! pyenv        :disable t)
+(package! hl-line      :disable t)
+(package! diredfl      :disable t)
+(package! haml-mode    :disable t)
+(package! lsp          :disable t)
+(package! powerline    :disable t)
+(package! popwin       :disable t)
+(package! ivy          :disable t)
+(package! swiper       :disable t)
+(package! org-yt       :disable t)
+(package! orgit-forge  :disable t)
 (package! org-pdftools :disable t)
 
 ;; Misc packages
