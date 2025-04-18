78885d2e07341e3aef419b1511e34011d18398d0
Andrew Peck <peckandrew@gmail.com>
2025-04-18 15:38:32 -0700

    cleanup lsp config

--- lisp/setup-lsp.el
+++ lisp/setup-lsp.el
@@ -1,37 +1,30 @@
+;; -*- lexical-binding: t; -*-
 ;;------------------------------------------------------------------------------
 ;; LSP
 ;;------------------------------------------------------------------------------
 
-(when (and (modulep! :tools lsp)
-           (not (modulep! :tools lsp +eglot)))
+(use-package lsp-mode
+  :when (modulep! :tools lsp -eglot)
 
-  (use-package lsp-mode
-    :config
+  :custom
 
-    (setopt lsp-file-watch-threshold 2000)
-    (setopt lsp-warn-no-matched-clients nil)
+  (lsp-auto-register-remote-clients nil) ; ?? does not seem to work ; https://github.com/doomemacs/doomemacs/issues/7491
+  (lsp-file-watch-threshold 2000)
+  (lsp-warn-no-matched-clients nil)
+  (lsp-enable-symbol-highlighting nil)
+  (lsp-vhdl-server 'hdl-checker)
+  ;; (lsp-vhdl-server-path "~/.local/bin/hdl_checker"); only needed if hdl_checker is not already on the PATH
 
-    (setopt lsp-enable-symbol-highlighting nil
-            lsp-vhdl-server 'hdl-checker
-            ;; lsp-vhdl-server-path "~/.local/bin/hdl_checker"; only needed if hdl_checker is not already on the PATH
-            )
+  :config
 
-    (add-to-list 'lsp-file-watch-ignored (expand-file-name "~/.pyenv"))
-    (add-to-list 'lsp-language-id-configuration '(verilog-mode . "verilog"))
-    (add-to-list 'lsp-disabled-clients 'svlangserver)
-    (add-to-list 'lsp-disabled-clients 'lsp-verilog-verible)
-
-    ;; (add-hook 'verilog-mode-hook 'lsp)
-    ;; (add-hook 'vhdl-mode-hook 'lsp)
-
-    ;; https://github.com/doomemacs/doomemacs/issues/7491
-    (setq lsp-auto-register-remote-clients nil) ; ?? does not seem to work
-    )
+  (add-to-list 'lsp-file-watch-ignored (expand-file-name "~/.pyenv"))
+  (add-to-list 'lsp-language-id-configuration '(verilog-mode . "verilog"))
+  (add-to-list 'lsp-disabled-clients 'svlangserver)
+  (add-to-list 'lsp-disabled-clients 'lsp-verilog-verible)
 
   ;;------------------------------------------------------------------------------
   ;; Booster
   ;;------------------------------------------------------------------------------
-
 
   (defun lsp-booster--advice-json-parse (old-fn &rest args)
     "Try to parse bytecode instead of json."
@@ -67,82 +60,37 @@
 ;; Eglot
 ;;------------------------------------------------------------------------------
 
-(when (modulep! :tools lsp +eglot)
-      (use-package! eglot
+(use-package! eglot
 
-        :init
+  :if (modulep! :tools lsp +eglot)
 
-        (setq eglot-managed-mode-hook
-              (list (lambda () (eldoc-mode -1))))
+  :custom
 
-        :config
- 
-        (add-to-list 'eglot-server-programs
-                     '((python-mode python-ts-mode)
-                       "basedpyright-langserver" "--stdio"))
+  (eglot-managed-mode-hook (list (lambda () (eldoc-mode -1))))
+  (eglot-events-buffer-config '(:size 2000000 :format full))
+  (eglot-prefer-plaintext nil)
+  (eglot-autoshutdown t)
+  (help-at-pt-display-when-idle t)
 
-        (defun emacs-lsp-booster-install ()
-          (interactive)
-          (compile "cargo install --git https://github.com/blahgeek/emacs-lsp-booster"))
+  :config
 
-        (unless (executable-find "emacs-lsp-booster")
-          (error "emacs-lsp booster not found! install with emacs-lsp-booster-install"))
+  (add-to-list 'eglot-server-programs
+               '((python-mode python-ts-mode)
+                 "basedpyright-langserver" "--stdio"))
 
-        (setq eglot-prefer-plaintext nil
-              eglot-autoshutdown t
-              help-at-pt-display-when-idle t)
+  (defun emacs-lsp-booster-install ()
+    (interactive)
+    (compile "cargo install --git https://github.com/blahgeek/emacs-lsp-booster"))
 
-        ;; (add-to-list 'eglot-server-programs '(python-mode . ("ruff-lsp")))
-        ;; (add-to-list 'eglot-server-programs '(python-ts-mode . ("ruff-lsp")))
-        ;; (add-to-list 'eglot-server-programs '(python-mode . ("pyright-langserver" "--stdio")))
-        ;; (add-to-list 'eglot-server-programs '(python-ts-mode . ("pyright-langserver" "--stdio")))
+  (unless (executable-find "emacs-lsp-booster")
+    (error "emacs-lsp booster not found! install with emacs-lsp-booster-install"))
 
-        ;; (add-hook! python-mode-hook
-        ;;   (setq eglot-workspace-configuration
-        ;;         '((pyright
-        ;;            (plugins
-        ;;             (mccabe (enabled . t))    ; Remove this if you want mccabe.
-        ;;             (pycodestyle (enabled . nil))
-        ;;             (flake8 (enabled . t)))))))
+  (add-to-list 'eglot-server-programs
+               '(vhdl-mode . ("ghdl-ls"))))
 
-        ;; (add-to-list 'eglot-server-programs '(python-mode . ("pyright-langserver" "--stdio")))
-        ;; (add-to-list 'eglot-server-programs '(python-ts-mode . ("pyright-langserver" "--stdio")))
-
-                                        ; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=61373
-        ;; pyright generates html :(
-        ;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=61373
-
-        ;; (dolist (provider '(:hoverProvider :documentHighlightProvider))
-        ;;   (add-to-list 'eglot-ignored-server-capabilities provider))
-
-
-        ;; (add-to-list 'eglot-workspace-configuration
-        ;;              '(:svlangserver (:settings (:systemverilog.launchConfiguration: "verilator -sv -Wall --lint-only",
-        ;;                                          :systemverilog.formatCommand: "verible-verilog-format"))))
-
-        ;; (add-to-list 'eglot-server-programs
-        ;;              '(verilog-mode . ("svls")))
-
-        ;; (add-hook! verilog-mode-hook
-        ;;   (setq eglot-workspace-configuration
-        ;;         `((:systemverilog
-        ;;            (:includeIndexing '["**/*.{sv,svh}"])
-        ;;            (:excludeIndexing '["test/**/*.{sv,svh}"])
-        ;;            (:defines nil)
-        ;;            (:launchConfiguration "verilator -sv --lint-only -Wall")
-        ;;            (:lintOnUnsaved t)
-        ;;            (:formatCommand "verible-verilog-format")
-        ;;            (:disableCompletionProvider nil)
-        ;;            (:disableHoverProvider nil)
-        ;;            (:disableSignatureHelpProvider nil)
-        ;;            (:disableLinting nil)))))
-
-        (add-to-list 'eglot-server-programs
-                     '(vhdl-mode . ("ghdl-ls"))))
-
-      (use-package! eglot-booster
-        :after eglot
-        :init
-        (cl-remprop 'buffer-local-value 'byte-obsolete-generalized-variable)
-        :config
-        (setq eglot-booster-no-remote-boost t)))
+(use-package! eglot-booster
+  :after eglot
+  :init
+  (cl-remprop 'buffer-local-value 'byte-obsolete-generalized-variable)
+  :custom
+  (eglot-booster-no-remote-boost t))
