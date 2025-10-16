6e9fb742d06f95dad22e832c15f8f6ecbfdd726b
Andrew Peck <peckandrew@gmail.com>
2025-10-15 21:54:44 -0700

    bump(resume)

--- packages.el
+++ packages.el
@@ -84,7 +84,7 @@
 (package! poporg                  :pin "2c58d68c81ecca4140bf179f19ed153ec804b65a" :recipe (:host github :repo "QBobWatson/poporg"))
 (package! rainbow-delimiters      :pin "f40ece58df8b2f0fb6c8576b527755a552a5e763")
 (package! rainbow-mode            :pin "2e6b18609c2fdd1a2dc513937a64d276fd6cf24c")
-(package! resume.el               :pin "40ba4f9334f62cffc8c94eaa384897c29b58ab5c" :recipe (:host github :repo "andrewpeck/resume.el"))
+(package! resume.el               :pin "f451785aae168ea4505846e38381e56f910076cc" :recipe (:host github :repo "andrewpeck/resume.el"))
 (package! scad-mode               :pin "b130730a3123387e69c85cf10633701ea447fa2a")
 (package! screenshot              :pin "2770c0cfefe1cc09d55585f4f2f336a1b26e610e" :recipe (:host github :repo "tecosaur/screenshot"))
 (package! standard-themes         :pin "0e94cf4e05a86fb0393fb7113986a105e6e469ea" :recipe (:host github :repo "protesilaos/standard-themes"))
