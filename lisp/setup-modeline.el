e094519ea5ac686703c96c3cb9fa95599f3b4801
Andrew Peck <peckandrew@gmail.com>
2025-10-21 19:28:06 -0700

    more modeline cleanup

--- lisp/setup-modeline.el
+++ lisp/setup-modeline.el
@@ -10,21 +10,16 @@
         nyan-bar-length 16
         nyan-wavy-trail t))
 
-
 (defvar modeline-show-flycheck-names nil)
 
 (defsubst modeline-flycheck-state ()
   ""
-  (string-trim
-   (let-alist (flycheck-count-errors flycheck-current-errors)
-     (if (not (or .error .warning))
-         ;; no errors or warnings
-         ""
-       ;; else
-       (concat
-        (propertize (format "%s" (or .error "0") ) 'face '(:inherit error))
-        "·"
-        (propertize (format "%s" (or .warning "0")) 'face '(:inherit warning)))))))
+  (if-let* ((status (flycheck-count-errors flycheck-current-errors)))
+      (let-alist status
+        (concat (propertize (format "%s" (or .error "0") ) 'face '(:inherit error))
+                "·"
+                (propertize (format "%s" (or .warning "0")) 'face '(:inherit warning))))
+    "")) ;; no errors or warnings
 
 (defsubst my-flycheck-mode-line-status-text (&optional status)
   "Get a text describing STATUS for use in the mode line.
@@ -53,63 +48,54 @@ nil."
   ;; https://emacs.stackexchange.com/questions/10955/customize-vc-mode-appearance-in-mode-line
   (advice-add #'vc-git-mode-line-string :filter-return #'advice/vc-mode-line-transform))
 
-(setq eglot-menu-string "⌁")
-(custom-set-faces '(eglot-mode-line ((t))))
+(setq-default mode-line-format
+              '(
+                ;;LEFT
+                ;; mode-line-front-space
+                "%e"
+                evil-mode-line-tag
+                mode-line-mule-info
+                "%* "
 
-(setq mode-line-format
-      '(
-        ;;LEFT
-        "%e"
-        evil-mode-line-tag
-        mode-line-mule-info
-        "%* "
+                (:eval (when-let* ((host (remote-host? default-directory)))
+                         (concat (propertize host 'face '(:inherit warning)) ":")))
 
-        (:eval (when-let* ((host (remote-host? default-directory)))
-                 (concat (propertize host 'face '(:inherit warning)) ":")))
+                (:eval (propertized-buffer-identification "%b"))
 
-        (:eval (propertized-buffer-identification "%b"))
+                (:eval (and nyan-mode
+                         (concat " " (nyan-create))))
+                
+                (:eval (and (or defining-kbd-macro executing-kbd-macro)
+                         (concat " (MACRO " (char-to-string evil-this-macro) ")")))
 
-        (:eval (when nyan-mode
-                 (concat " " (nyan-create))))
+                ;; RIGHT PAD
+                mode-line-format-right-align
 
-        ;; RIGHT PAD
-        mode-line-format-right-align
+                ;; RIGHT
 
-        ;; RIGHT
+                ;; venv
+                (:eval (and buffer-env-active " "))
 
-        (:eval
-         (string-join 
-          (remq nil 
-                (list
-                 (when (or defining-kbd-macro executing-kbd-macro)
-                   (concat "MACRO(" (char-to-string evil-this-macro) ") ⋅"))
+                ;; lsp
+                (:eval (and (fboundp #'eglot-managed-p)
+                            (eglot-managed-p)) " ")
 
-                 (and flycheck-mode flycheck-enabled-checkers
-                      (concat (when modeline-show-flycheck-names
-                                (concat
-                                 "("
-                                 (string-join (mapcar 'symbol-name flycheck-enabled-checkers) " ") " "))
+                ;; flycheck
+                (:eval (and flycheck-mode flycheck-enabled-checkers
+                            (concat (when modeline-show-flycheck-names
+                                      (concat
+                                       "("
+                                       (string-join (mapcar 'symbol-name flycheck-enabled-checkers) " ") " "))
 
-                              (my-flycheck-mode-line-status-text)
+                                    (my-flycheck-mode-line-status-text)
 
-                              (when modeline-show-flycheck-names ")")))
+                                    (when modeline-show-flycheck-names ")") " ")))
 
-                 (pcase major-mode
-                   ('pdf-view-mode (format "%s / %s" (pdf-view-current-page) (pdf-cache-number-of-pages)))
-                   (_  "(L%l C%c %p)"))
+                ;; position
+                (:eval (concat (pcase major-mode
+                                 ('pdf-view-mode (format "%s / %s" (pdf-view-current-page) (pdf-cache-number-of-pages)))
+                                 (_  "(L%l C%c %p)")) " "))
 
-                 (when (not (remote-host? default-directory))
-                   (when-let ((m vc-mode))
-                     (concat "(" (string-trim m) ")")))
-
-                 ;; (format "%s" (if (listp mode-name) (car mode-name) mode-name))
-
-                 (when buffer-env-active "")
-
-                 ;; replace (eglot--mode-line-format)
-                 (when (and (fboundp #'eglot-managed-p)
-                            (eglot-managed-p)) "")))
-          " "))
-
-        ;; END
-        " " mode-line-end-spaces))
+                ;; git
+                (:eval (when-let* ((m (and (not (remote-host? default-directory)) vc-mode)))
+                         (concat "(" (string-trim m) ")" " ")))))
