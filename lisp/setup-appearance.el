18afd4431b7ec63f27ce65b41ea3e7c11f0a079c
Andrew Peck <peckandrew@gmail.com>
2025-10-22 20:11:24 -0700

    use use-package for theme config

--- lisp/setup-appearance.el
+++ lisp/setup-appearance.el
@@ -34,7 +34,7 @@
 (defvar ap/light-theme
   (if (not (display-graphic-p)) 'summerfruit
     (pcase (downcase (system-name))
-      ("apeck-len01"  'standard-light)
+      ("apeck-len01"  'modus-operandi)
       ("pepper"       'doom-one-light)
       ("larry"        'summerfruit)
       ("strange"      'summerfruit)
@@ -50,89 +50,101 @@
 ;; Theme Customization
 ;;------------------------------------------------------------------------------
 
-(run-when-idle
- 0.1
- ;; override some annoying defaults of doom-gruvbox
- (custom-theme-set-faces! 'doom-gruvbox
-   ;; the default function name face is the same as the string face and looks awful for lisp
-   '(font-lock-function-name-face :foreground "#d3869b")
-   '(whitespace-tab :background "#282828")
-   '(org-todo :weight bold :foreground "orange red")
-   '(org-table :weight normal :foreground "lightblue")
-   '(org-link :weight normal :underline "lightpink" :foreground "light pink")
-   '(org-level-1 :height 1.5 :weight bold   :foreground "#b8bb26")
-   '(org-level-2 :height 1.2 :weight bold   :foreground "lightblue")
-   '(org-level-3 :height 1.1 :weight normal :foreground "#d3869b")
-   '(org-level-4 :height 1.1 :weight normal :foreground "#83a598"))
+(use-package doom-gruvbox-theme
+  :config
+  ;; override some annoying defaults of doom-gruvbox
+  (custom-theme-set-faces! 'doom-gruvbox
+    ;; the default function name face is the same as the string face and looks awful for lisp
+    '(font-lock-function-name-face :foreground "#d3869b")
+    '(mu4e-unread-face :foreground "#cccccc" :weight bold)
+    '(whitespace-tab :background "#282828")
+    '(org-todo :weight bold :foreground "orange red")
+    '(org-table :weight normal :foreground "lightblue")
+    '(org-link :weight normal :underline "lightpink" :foreground "light pink")
+    '(org-level-1 :height 1.5 :weight bold   :foreground "#b8bb26")
+    '(org-level-2 :height 1.2 :weight bold   :foreground "lightblue")
+    '(org-level-3 :height 1.1 :weight normal :foreground "#d3869b")
+    '(org-level-4 :height 1.1 :weight normal :foreground "#83a598")))
 
- (custom-theme-set-faces! 'doom-spacegrey
-   '(org-todo :weight bold :foreground "orange red")
-   '(org-table :weight normal :foreground "lightblue")
-   '(org-link :weight normal :underline "lightpink" :foreground "light pink")
-   '(org-level-1 :height 1.5 :weight bold   :foreground "#c0c5ce")
-   '(org-level-2 :height 1.2 :weight bold   :foreground "lightblue")
-   '(org-level-3 :height 1.1 :weight normal :foreground "#d3869b")
-   '(org-level-4 :height 1.1 :weight normal :foreground "#83a598"))
+(use-package doom-spacegray-theme
+  :config
+  (custom-theme-set-faces! 'doom-spacegrey
+    '(org-todo :weight bold :foreground "orange red")
+    '(org-table :weight normal :foreground "lightblue")
+    '(org-link :weight normal :underline "lightpink" :foreground "light pink")
+    '(org-level-1 :height 1.5 :weight bold   :foreground "#c0c5ce")
+    '(org-level-2 :height 1.2 :weight bold   :foreground "lightblue")
+    '(org-level-3 :height 1.1 :weight normal :foreground "#d3869b")
+    '(org-level-4 :height 1.1 :weight normal :foreground "#83a598")))
 
- (custom-theme-set-faces! 'hima
-   '(org-level-1 :height 1.0 :weight bold)
-   '(org-level-2 :height 1.0 :weight bold)
-   '(org-level-3 :height 1.0 :weight bold)
-   '(org-level-4 :height 1.0 :weight bold))
 
- (custom-theme-set-faces! 'standard-light
-   '(font-latex-sectioning-0-face :height 2.0 :weight bold)
-   '(font-latex-sectioning-1-face :height 1.8 :weight bold)
-   '(font-latex-sectioning-2-face :height 1.3 :weight bold)
-   '(font-latex-sectioning-3-face :height 1.1 :weight bold)
-   '(font-latex-sectioning-4-face :height 1.0 :weight bold))
+(use-package hima-theme
+  :config
+  (custom-theme-set-faces! 'hima
+    '(org-level-1 :height 1.0 :weight bold)
+    '(org-level-2 :height 1.0 :weight bold)
+    '(org-level-3 :height 1.0 :weight bold)
+    '(org-level-4 :height 1.0 :weight bold)
+    '(font-latex-italic-face :inherit default :slant italic)
+    '(font-latex-bold-face   :inherit default :weight bold)))
 
- (custom-theme-set-faces! 'modus-operandi
-   '(font-latex-sectioning-0-face :height 2.0 :weight bold)
-   '(font-latex-sectioning-1-face :height 1.8 :weight bold)
-   '(font-latex-sectioning-2-face :height 1.4 :weight bold)
-   '(font-latex-sectioning-3-face :height 1.2 :weight bold)
-   '(font-latex-sectioning-4-face :height 1.0 :weight bold))
+(use-package standard-light-theme
+  :config
+  (custom-theme-set-faces! 'standard-light
+    '(fringe :inherit background)
+    '(mode-line :background "#ddd")
+    '(mu4e-unread-face :foreground "black" :weight bold)
+    '(font-latex-sectioning-0-face :height 2.0 :weight bold)
+    '(font-latex-sectioning-1-face :height 1.8 :weight bold)
+    '(font-latex-sectioning-2-face :height 1.3 :weight bold)
+    '(font-latex-sectioning-3-face :height 1.1 :weight bold)
+    '(font-latex-sectioning-4-face :height 1.0 :weight bold)))
 
- (custom-theme-set-faces! 'solarized-zenburn
-   '(font-lock-doc-face :inherit font-lock-comment-face)
-   '(font-lock-string-face :inherit error)
-   '(font-lock-keyword-face :foreground "#7F9F7F")
-   '(font-lock-constant-face :foreground "#7F9F7F")
-   '(font-lock-variable-name-face :foreground "#8CD0D3" :weight normal)
-   '(flyspell-incorrect :underline (:style wave :color "#CC9393") )
-   '(flycheck-error :underline (:style wave :color "#552424") )
-   '(flycheck-info :underline (:style wave :color "#75898a") )
-   '(highlight-quoted-symbol :inherit font-lock-variable-name-face :slant italic)
-   '(font-latex-italic-face :inherit default :slant italic)
-   '(font-latex-bold-face :inherit default :weight bold))
+(use-package modus-operandi-theme
+  :config
+  (custom-theme-set-faces! 'modus-operandi
+    '(font-latex-sectioning-0-face :height 2.0 :weight bold)
+    '(font-latex-sectioning-1-face :height 1.8 :weight bold)
+    '(font-latex-sectioning-2-face :height 1.4 :weight bold)
+    '(font-latex-sectioning-3-face :height 1.2 :weight bold)
+    '(font-latex-sectioning-4-face :height 1.0 :weight bold)
+    '(nobreak-space :inherit modeline)
+    '(hl-todo :inherit success)))
 
- (let ((fg "#888"))
-   (custom-theme-set-faces! 'almost-mono-black
-     `(font-lock-comment-face :foreground "#666")
-     `(font-lock-keyword-face :foreground "#ccc")
-     `(font-lock-variable-name-face :foreground "#cecece")
-     `(default :foreground "#888")))
+(use-package solarized-zenburn-theme
+  :config
+  (custom-theme-set-faces! 'solarized-zenburn
+    '(font-lock-doc-face :inherit font-lock-comment-face)
+    '(font-lock-string-face :inherit error)
+    '(font-lock-keyword-face :foreground "#7F9F7F")
+    '(font-lock-constant-face :foreground "#7F9F7F")
+    '(font-lock-variable-name-face :foreground "#8CD0D3" :weight normal)
+    '(flyspell-incorrect :underline (:style wave :color "#CC9393") )
+    '(flycheck-error :underline (:style wave :color "#552424") )
+    '(flycheck-info :underline (:style wave :color "#75898a") )
+    '(highlight-quoted-symbol :inherit font-lock-variable-name-face :slant italic)
+    '(font-latex-italic-face :inherit default :slant italic)
+    '(font-latex-bold-face :inherit default :weight bold)))
 
- (custom-theme-set-faces! 'doom-zenburn
-   '(font-latex-italic-face :inherit default :slant italic)
-   '(font-latex-bold-face :inherit default :weight bold))
+(use-package almost-mono-black-theme
+  :config
+  (custom-theme-set-faces! 'almost-mono-black
+    `(font-lock-comment-face :foreground "#666")
+    `(font-lock-keyword-face :foreground "#ccc")
+    `(font-lock-builtin-face :foreground "#ccc")
+    `(font-lock-variable-name-face :foreground "#cecece")
+    `(highlight :foreground "#ddd" :background "#3c5e2b")
+    `(region    :inherit 'highlight)
+    `(default :foreground "#ccc" :background "#080808")))
 
- ;; HACK for using nobreak-space in modeline
- (custom-theme-set-faces! 'doom-spacegrey
-   '(nobreak-space :inherit modeline))
+(use-package doom-zenburn-theme
+  :config
+  (custom-theme-set-faces! 'doom-zenburn
+    '(font-latex-italic-face :inherit default :slant italic)
+    '(font-latex-bold-face :inherit default :weight bold)))
 
- (custom-theme-set-faces! 'hima
-   ;; '(font-lock-type-face :inherit default :family "Courier New" )
-   '(font-latex-italic-face :inherit default :slant italic)
-   '(font-latex-bold-face :inherit default :weight bold))
-
- (custom-theme-set-faces! 'modus-operandi
-   '(nobreak-space :inherit modeline))
-
- (custom-theme-set-faces! 'modus-operandi
-   '(hl-todo :inherit success))
-
- (custom-theme-set-faces! 'standard-light
-   '(fringe :inherit background)
-   '(mode-line :background "#ddd")))
+(use-package doom-spacegray-theme
+  :config
+  ;; HACK for using nobreak-space in modeline
+  (custom-theme-set-faces! 'doom-spacegrey
+    '(nobreak-space :inherit modeline)))
