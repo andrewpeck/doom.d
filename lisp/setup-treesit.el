a6f1d0e3a86349922ae931242e90507ba778b4e2
Andrew Peck <peckandrew@gmail.com>
2026-03-05 14:26:09 -0800

    whitespace

--- lisp/setup-treesit.el
+++ lisp/setup-treesit.el
@@ -14,16 +14,12 @@
 
   (defun treesit-explore-start ()
     (interactive)
-
     (add-hook 'post-command-hook #'treesit--explorer-post-command 0 t)
-
     (treesit-explore-mode t)
-
     (treesit--explorer-refresh)
-
     ;; this should happen automatically, something is wrong, buffer local hooks or something?
     ;; some other hook overwriting it?
-    (treesit--explorer-post-command))
+    (treesit--explorer-post-command)))
 
   (define-derived-mode verilog-ts-mode verilog-mode "Verilog"
     "A mode for Verilog."
