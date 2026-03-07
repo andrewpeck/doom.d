7c545be6035205ec8e9627870fac43cf811a1485
Andrew Peck <peckandrew@gmail.com>
2026-03-06 16:26:37 -0800

    bump(affe)

--- packages.el
+++ packages.el
@@ -49,9 +49,9 @@
 ;; Misc packages
 ;; start:sort
 (package! adaptive-wrap           :pin "d75665b9c88e65f91dadd1e5880905bbdb7c09b7")
-(package! affe                    :pin "10fc401cf2d35bafb7220b9456af7dbfd064e1f2")
 (package! almost-mono-themes      :pin "0641bf565c113caef8d5c2a93f38cff32ebb62b7" :recipe (:host github :repo "cryon/almost-mono-themes"))
 (package! apheleia                :pin "f1d36031fc162f541f77d4f6c6c0fd6197aba077")
+(package! affe                    :pin "295e2fb26a2de66e13c0f8414d1ada5b090a1011")
 (package! ascii-art-to-unicode    :pin "fa3d82ddb531855ad7b8ef871871907861327c9f")
 (package! benchmark-init          :pin "6507caa3c4cb2a6c9b85c771c5e9e5aeb7d745bc")
 (package! browse-at-remote        :pin "cf0269f3db9e968c819b1d85b33d791c20c2e495" :recipe (:host github :repo "rmuslimov/browse-at-remote"))
