fade77d5a7da5fe6f282dc3da36e206c8ff00284
Andrew Peck <peckandrew@gmail.com>
2026-03-13 10:09:58 -0700

    update dashboard

--- lisp/setup-doom.el
+++ lisp/setup-doom.el
@@ -1,7 +1,4 @@
 ;; -*- lexical-binding: t; -*-
-;;------------------------------------------------------------------------------
-;; Doom
-;;------------------------------------------------------------------------------
 
 (defun doom/package-under-review ()
   "Open the doom packages under review Github page"
@@ -9,57 +6,20 @@
   (browse-url "https://github.com/orgs/doomemacs/projects/5/views/1"))
 
 ;; Dashboard
-(defun ap/dashboard-widget-banner ()
-  (let ((point (point)))
-    (mapc (lambda (line)
-            (insert (propertize (+doom-dashboard--center +doom-dashboard--width line)
-                                'face 'doom-dashboard-banner) " ")
-            (insert "\n"))
-          '(""
-            ;; "    ↑      "
-            ;; "← h j k l →"
-            ;; "      ↓    "
-            )
-          )
+(setq +dashboard-ascii-banner-fn (lambda () "Emacs"))
 
-    (when (and (display-graphic-p)
-               (stringp fancy-splash-image)
-               (file-readable-p fancy-splash-image))
-      (let ((image (create-image (fancy-splash-image-file))))
-        (add-text-properties
-         point (point) `(display ,image rear-nonsticky (display)))
-        (save-excursion
-          (goto-char point)
-          (insert (make-string
-                   (truncate
-                    (max 0 (+ 1 (/ (- +doom-dashboard--width
-                                      (car (image-size image nil)))
-                                   2))))
-                   ? ))))
-      (insert (make-string (or (cdr +doom-dashboard-banner-padding) 0)
-                           ?\n)))))
+(setq +dashboard-functions
+  `(+dashboard-widget-banner
+    +dashboard-widget-loaded))
 
-(defun doom-display-benchmark-h (&optional return-p)
-  "Display a benchmark including number of packages and modules loaded.
-
-If RETURN-P, return the message as a string instead of displaying it."
-  (funcall (if return-p #'format #'message)
-           "Loaded %d packages across %d modules in %.03fs"
-           (- (length load-path) (length (get 'load-path 'initial-value)))
-           (hash-table-count doom-modules)
-           doom-init-time))
-
-(setq +doom-dashboard-functions
-      '(ap/dashboard-widget-banner
-        ;; doom-dashboard-widget-shortmenu
-        doom-dashboard-widget-loaded
-        ;; doom-dashboard-widget-footer
-        ))
-
-;;
 (remove-hook 'doom-first-buffer-hook #'smartparens-global-mode)
 
-(setq doom-scratch-dir doom-user-dir)
+;; add an extra newline to center it, maybe should report
+(defun +dashboard-widget-loaded ()
+  (when doom-init-time
+    (+dashboard-insert-centered
+     (concat "\n"
+             (propertize (doom-display-benchmark-h 'return)
+                         'face '+dashboard-loaded)))))
 
-;; (remove-hook
-;;  '+doom-dashboard-functions #'doom-dashboard-widget-shortmenu)
+(setq doom-scratch-dir doom-user-dir)
