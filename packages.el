f4290023983d00772cec9cac9a446fa9b0e97874
Andrew Peck <peckandrew@gmail.com>
2026-04-12 13:43:30 -0700

    bump(standard-themes)

--- packages.el
+++ packages.el
@@ -107,7 +107,7 @@
 (package! rainbow-mode            :pin "2e6b18609c2fdd1a2dc513937a64d276fd6cf24c")
 (package! scad-mode               :pin "44481331536516e34940315f67fa58f99f62a589")
 (package! screenshot              :pin "2770c0cfefe1cc09d55585f4f2f336a1b26e610e" :recipe (:host github :repo "tecosaur/screenshot"))
-(package! standard-themes         :pin "8c24caa42b369077ccb7b2c0111084fb2153d5a4" :recipe (:host github :repo "protesilaos/standard-themes"))
+(package! standard-themes         :pin "7c8e27d5884671bc674ecfe83fb0e66a338ceb70" :recipe (:host github :repo "protesilaos/standard-themes"))
 (package! string-edit-at-point    :pin "87936d816ae24184dd83688136531b6b6f1943fe" :recipe (:host github :repo "emacsmirror/string-edit-at-point"))
 (package! svg-tag-mode            :pin "13e888b8bd9a0664d060149a44a751b2113331b6" :recipe (:host github :repo "rougier/svg-tag-mode"))
 (package! systemd                 :pin "8742607120fbc440821acbc351fda1e8e68a8806" :recipe (:host github :repo "holomorph/systemd-mode"))
