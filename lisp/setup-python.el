92b9527979537cfe245288cd228a8c7a5e15e124
Andrew Peck <peckandrew@gmail.com>
2026-08-19 15:56:22 -0700

    improve venv script

--- lisp/setup-python.el
+++ lisp/setup-python.el
@@ -68,7 +68,7 @@ or for a single package (uv-install \"mypy\")
 
     (unless (getenv "VIRTUAL_ENV")
       (when (yes-or-no-p "Virtual environment not active. Do you want to create at venv project root?")
-        (when (shell-command (concat "uv venv --python 3.12 " (doom-project-root) ".venv"))
+        (when (compile (concat "uv venv --python 3.12 --clear" (doom-project-root) ".venv"))
 
           (let* ((file (concat (doom-project-root) ".venv/bin/activate"))
                  (hash (with-temp-buffer
